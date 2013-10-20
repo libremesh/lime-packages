4870ffaeae8656830d3c300cdcd85eb2fb7c0daf
Gui Iribarren <gui@altermundi.net>
2013-10-20 10:22:00 +0200

    refactor set_hostname: separate configure from apply action

--- packages/lime-system/files/usr/lib/lua/lime/system.lua
+++ packages/lime-system/files/usr/lib/lua/lime/system.lua
@@ -1,17 +1,5 @@
 #!/usr/bin/lua
 
-local function set_hostname()
-	local r1, r2, r3 = node_id()
-	local hostname = string.format("%02x%02x%02x", r1, r2, r3)
-
-	x:foreach("system", "system", function(s)
-		x:set("system", s[".name"], "hostname", hostname)
-	end)
-	x:save("system")
-
-	fs.writefile("/proc/sys/kernel/hostname", hostname)
-end
-
 function configure()
 	print("Configuring system...")
 	set_hostname()
@@ -22,3 +10,24 @@ function configure()
 	x:save("uhttpd")
 end
 
+function apply()
+  -- apply hostname
+  local hostname
+	x:foreach("system", "system", function(s)
+    hostname = x:get("system", s[".name"], "hostname")
+	end)
+  fs.writefile("/proc/sys/kernel/hostname", hostname)
+
+  -- apply uhttpd settings
+  os.execute("/etc/init.d/uhttpd reload")
+end
+
+local function set_hostname()
+	local r1, r2, r3 = node_id()
+	local hostname = string.format("%02x%02x%02x", r1, r2, r3)
+
+	x:foreach("system", "system", function(s)
+		x:set("system", s[".name"], "hostname", hostname)
+	end)
+	x:save("system")
+end
