9f16b2b03051be5d834f28c78940d9ad3e02b0b8
Gioacchino Mazzurco <gio@eigenlab.org>
2013-11-01 18:43:30 +0100

    Sanitized alfred output parsing

--- packages/lime-dnsmasq-lease-share/src/dnsmasq-lease-share.lua
+++ packages/lime-dnsmasq-lease-share/src/dnsmasq-lease-share.lua
@@ -17,6 +17,8 @@ along with this file. If not, see <http://www.gnu.org/licenses/>.
 
 ]]--
 
+-- PLEASE USE TAB NOT SPACE JUST FOR INDENTATION
+
 
 --! dhcp lease file lines format
 --! <Time of lease expiry, in epoch time (seconds since 1970)> <Client MAC Address> <Client IP> <Client unqualified hostname if provided, * if not provided> <Client-ID, if known. The client-ID is used as the computer's unique-ID in preference to the MAC address, if it's available>
@@ -36,50 +38,58 @@ local client_mac = arg[2];
 
 --! Tell alfred local dhcp lease changed
 function update_alfred()
-    local lease_file = io.open(local_lease_file, "r+");
-    local stdin = io.popen("afred -r " .. alfred_shared_lease_num,"w");
-    stdin.write(lease_file:read("*all"));
+	local lease_file = io.open(local_lease_file, "r+");
+	local stdin = io.popen("afred -r " .. alfred_shared_lease_num,"w");
+	stdin.write(lease_file:read("*all"));
+	stdin:close();
 end
 
 if command == "add" then
-    local lease_expiration = os.getenv("DNSMASQ_LEASE_EXPIRES");
-    local client_ip = arg[3];
-    local client_hostname;
-    if arg[4]:len() > 0 then client_hostname = arg[4] else client_hostname = "*" end;
-    local client_id = os.getenv("DNSMASQ_CLIENT_ID");
-    if client_id:len() <= 0 then client_id = client_mac end; 
+	local lease_expiration = os.getenv("DNSMASQ_LEASE_EXPIRES");
+	local client_ip = arg[3];
+	local client_hostname;
+	if arg[4]:len() > 0 then client_hostname = arg[4] else client_hostname = "*" end;
+	local client_id = os.getenv("DNSMASQ_CLIENT_ID");
+	if client_id:len() <= 0 then client_id = client_mac end; 
 
-    local lease_line = lease_expiration .. " " .. client_mac .. " " .. client_ip .. " " .. client_hostname .. " " .. client_id;
+	local lease_line = lease_expiration .. " " .. client_mac .. " " .. client_ip .. " " .. client_hostname .. " " .. client_id;
 
-    local lease_file = io.open(local_lease_file, "a");
-    lease_file:write(lease_file);
+	local lease_file = io.open(local_lease_file, "a");
+	lease_file:write(lease_file);
 
-    update_alfred()
+	update_alfred()
 
 elseif command == "del" then
-    local leases;
-    local lease_file = io.open(local_lease_file, "r");
-    while lease_file:read(0) do
-        local lease_line = lease_file:read();
-        if not string.find(lease_line, client_mac) then leases = leases .. lease_line .. "\n" end 
-    end
-    lease_file:close()
-    lease_file = io.open(local_lease_file, "w");
-    lease_file:write(leases);
-    lease_file:close();
-    update_alfred();
+	local leases;
+	local lease_file = io.open(local_lease_file, "r");
+	while lease_file:read(0) do
+		local lease_line = lease_file:read();
+		if not string.find(lease_line, client_mac) then leases = leases .. lease_line .. "\n" end 
+	end
+	lease_file:close()
+	lease_file = io.open(local_lease_file, "w");
+	lease_file:write(leases);
+	lease_file:close();
+	update_alfred();
     
 elseif command == "init" then
-    local stdout = io.popen("afred -r " .. alfred_shared_lease_num,"r");
-    local json_stdout;
-    local lease_table = {};
-    -------------------------------- { added because alfred doesn't output valid json yet }
-    assert(loadstring("json_stdout = {" .. stdout .. "}"))()
-    
-    for _, row in ipairs(json_stdout) do
-	    local node_mac, value = unpack(row)
-	    table.insert(lease_table, value:gsub("\x0a", "\n") .. "\n")
-    end
-    
-    print(table.concat(lease_table));
+	local stdout = io.popen("alfred -r " .. alfred_shared_lease_num,"r");
+	local raw_output = stdout:read("*a");
+	stdout:close();
+	if raw_output
+		then
+			local json_stdout = {};
+			local lease_table = {};
+			-------------------------------- { added because alfred doesn't output valid json yet }
+			assert(loadstring("json_stdout = {" .. raw_output .. "}"))()
+
+			for _, row in ipairs(json_stdout) do
+				local node_mac, value = unpack(row)
+				table.insert(lease_table, value:gsub("\x0a", "\n") .. "\n")
+			end
+			
+			print(table.concat(lease_table));
+		else
+			print("");
+	end
 end
