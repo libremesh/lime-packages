ac9b8a014d5ec32c178378e9cf39e039df92c30b
Gioacchino Mazzurco <gio@eigenlab.org>
2014-01-18 00:36:51 +0100

    Begin implementation of new config semantic

--- packages/lime-system/files/usr/lib/lua/lime/config.lua
+++ packages/lime-system/files/usr/lib/lua/lime/config.lua
@@ -7,5 +7,9 @@ config = {}
 config.uci = libuci:cursor()
 
 function config.get(sectionname, option, default)
-	return config:uci:get("lime", sectionname, option) or config:uci:get("lime-defaults", sectionname, option) or default 
+	return config.uci:get("lime", sectionname, option) or config.uci:get("lime-defaults", sectionname, option) or default 
+end
+
+function config.foreach(configtype, callback)
+	config.uci:foreach("lime", configtype, callback)
 end
