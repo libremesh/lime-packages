cceb58d38f23da6e69aaa0e3f329d26017199ae6
Juli <juli@deltalibre.org.ar>
2014-02-03 02:13:18 -0300

    pull juli source code changes

--- packages/vale/src/vale.h
+++ packages/vale/src/vale.h
@@ -4,7 +4,9 @@
 #define VALE_CHARS (VALE_BITS+5)/5
 #define VALE_KEY_BITS 128
 #define DATE_FORMAT "%Y-%m-%d %H:%M:%S"
-
+#define CLIENTS_FILE "./clients"
+#define VALE_USED_FILE "./vale_used"
+#define CONFIG_FILE "./config"
 
 
 unsigned char charset[] = "abcdefghijklmnopqrstuvwxyz234567";
@@ -37,12 +39,16 @@ void genb16table();
 int b32dec(char *in, unsigned char **outp);
 int b16dec(char *in, unsigned char **outp);
 vale * vale_decode(unsigned char *valebits);
-void vale_used_insert(vale_used *used, char * client_id, char *valestr);
+void vale_used_insert(vale_used *used, vale_used *vale_used);
+vale_used *vale_used_new(char *client_id,char *valestr);
 void print_vale_used(FILE * out, vale_used * used);
 unsigned char * vale_getkey(vale *v, group *groups);
+time_t vale_used_is(vale_used *used, char *valestr);
+vale_used *vale_used_new(char *client_id,char *valestr);
 int vale_isauth(unsigned char *data, unsigned char *key);
 int save_vale_used(char *path, vale_used *used);
 int save_clients(char *path, client * clients);
+char *vale_canon(char *in);
 
 char *load_file(char *path);
 group *load_groups();
@@ -52,7 +58,8 @@ vale_used *load_vale_used();
 void print_clients(FILE *out, client *clients);
 client * client_getbyid(client *clients, char *id);
 void client_insert(client *clients, client *newclient);
-void client_insertvale(char *id, vale * v, client * clients);
+void client_insertvale(client * clients, char *id, time_t expire);
+time_t my_timegm(struct tm *tm);
 
 #endif /* __VALE_H_ */
 
