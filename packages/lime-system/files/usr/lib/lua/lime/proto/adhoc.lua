3293456f2ac2f220acfb2a7be16558e6bbe3ab02
Gioacchino Mazzurco <gio@eigenlab.org>
2014-02-05 14:27:43 +0100

    avoid race condition by better modularization, adhoc interface created correctly

---
+++ packages/lime-system/files/usr/lib/lua/lime/proto/adhoc.lua
@@ -0,0 +1,23 @@
+#!/usr/bin/lua
+
+local adhoc = {}
+
+function adhoc.configure() end
+
+function adhoc.setup_interface(ifname, args)
+	if ifname:match("^wlan%d_adhoc") then
+		local libuci = require "uci"
+		local uci = libuci:cursor()
+
+		local networkInterfaceName = network.limeIfNamePrefix..ifname
+
+		uci:set("network", networkInterfaceName, "interface")
+		uci:set("network", networkInterfaceName, "proto", "none")
+		uci:set("network", networkInterfaceName, "mtu", "1536")
+		uci:set("network", networkInterfaceName, "auto", "1")
+
+		uci:save("network")
+	end
+end
+
+return adhoc
