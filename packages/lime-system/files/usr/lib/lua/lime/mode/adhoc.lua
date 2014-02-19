abee6016f0dd1c64ae3ab8d59bd5970a20af2f4a
Gioacchino Mazzurco <gio@eigenlab.org>
2014-02-19 22:23:25 +0100

    Remove unnecessary assignations

--- packages/lime-system/files/usr/lib/lua/lime/mode/adhoc.lua
+++ packages/lime-system/files/usr/lib/lua/lime/mode/adhoc.lua
@@ -7,7 +7,7 @@ adhoc.wifi_mode="adhoc"
 function adhoc.setup_radio(radio, args)
 --!	checks("table", "?table")
 
-	local wifi_iface = wireless.createBaseWirelessIface(radio, adhoc.wifi_mode, nil)
+	wireless.createBaseWirelessIface(radio, adhoc.wifi_mode, nil)
 end
 
 return adhoc
