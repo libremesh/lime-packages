3714dee3ec09eeb7222cf797c30b14edbb63138e
Gioacchino Mazzurco <gio@eigenlab.org>
2014-02-19 22:23:25 +0100

    Remove unnecessary assignations

--- packages/lime-system/files/usr/lib/lua/lime/mode/ap.lua
+++ packages/lime-system/files/usr/lib/lua/lime/mode/ap.lua
@@ -7,8 +7,7 @@ ap.modename="ap"
 function ap.setup_radio(radio, args)
 --!	checks("table", "?table")
 
-	local extras = { network = "lan" }
-	local wifi_iface = wireless.createBaseWirelessIface(radio, ap.wifi_mode, extras)
+	wireless.createBaseWirelessIface(radio, ap.wifi_mode, { network = "lan" })
 end
 
 return ap
