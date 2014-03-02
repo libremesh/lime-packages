d4c3c0437aa60f3aaf39b7d2307051bd52d4411a
Gioacchino Mazzurco <gio@eigenlab.org>
2014-03-02 23:54:51 +0100

    dnsmasq config cleanup

--- packages/lime-proto-anygw/src/anygw.lua
+++ packages/lime-proto-anygw/src/anygw.lua
@@ -46,15 +46,15 @@ function anygw.configure(args)
 	-- IPv6 router advertisement for anygw interface
 	print("Enabling RA in dnsmasq...")
 	local content = { }
-	table.insert(content,               "enable-ra")
-	table.insert(content, string.format("dhcp-range=tag:anygw, %s, ra-names", anygw_ipv6:network(64):string()))
-	table.insert(content,               "dhcp-option=tag:anygw, option6:domain-search, lan")
-	table.insert(content, string.format("address=/anygw/%s", anygw_ipv6:host():string()))
-	table.insert(content, string.format("dhcp-option=tag:anygw, option:router, %s", anygw_ipv4:host():string()))
-	table.insert(content, string.format("dhcp-option=tag:anygw, option:dns-server, %s", anygw_ipv4:host():string()))
-	table.insert(content,               "dhcp-broadcast=tag:anygw")
-	table.insert(content,               "no-dhcp-interface=br-lan")
-	fs.writefile("/etc/dnsmasq.conf", table.concat(content, "\n").."\n")
+	table.insert(content, "enable-ra")
+	table.insert(content, "dhcp-range=tag:anygw,::,constructor:anygw,ra-names")
+	table.insert(content, "dhcp-option=tag:anygw,option6:domain-search,lan")
+	table.insert(content, "address=/anygw/"..anygw_ipv6:host():string())
+	table.insert(content, "dhcp-option=tag:anygw, option:router,"..anygw_ipv4:host():string())
+	table.insert(content, "dhcp-option=tag:anygw, option:dns-server,"..anygw_ipv4:host():string())
+	table.insert(content, "dhcp-broadcast=tag:anygw")
+	table.insert(content, "no-dhcp-interface=br-lan")
+	fs.writefile("/etc/dnsmasq.conf", table.concat(content, "\n").."\n") -- WhyTF we do 'table.concat(content, "\n").."\n"' and not 'table.concat(content, "\n\n")' ?
 
 	-- and disable 6relayd
 	print("Disabling 6relayd...")
