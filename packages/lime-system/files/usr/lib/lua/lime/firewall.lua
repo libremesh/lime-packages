acb2a0bb9a7e9b7ae83d4f35e11c1b7ec2aa82b1
Gioacchino Mazzurco <gio@eigenlab.org>
2014-03-20 03:57:34 +0100

    generalized and absorbed lime-firewall into lime-system

--- packages/lime-system/files/usr/lib/lua/lime/firewall.lua
+++ packages/lime-system/files/usr/lib/lua/lime/firewall.lua
@@ -5,7 +5,7 @@ local fs = require("nixio.fs")
 firewall = {}
 
 function firewall.clean()
-	local fwcfg = { "/etc/config/firewall", "/etc/lime-firewall.d/10-accept.start", "/etc/lime-firewall.d/15-mss_clamp.start" }
+	local fwcfg = { "/etc/config/firewall", "/etc/lime-init.d/10-accept.start", "/etc/lime-init.d/15-mss_clamp.start" }
 	for _,file in pairs(fwcfg) do
 		fs.writefile(file,"")
 	end
@@ -14,7 +14,7 @@ end
 function firewall.configure()
 
 	fs.writefile(
-		"/etc/lime-firewall.d/10-accept.start", [[
+		"/etc/lime-init.d/10-accept.start", [[
 iptables -P INPUT ACCEPT
 iptables -P OUTPUT ACCEPT
 iptables -P FORWARD ACCEPT
@@ -25,7 +25,7 @@ ip6tables -P FORWARD ACCEPT
 ]])
 
 	fs.writefile(
-		"/etc/lime-firewall.d/15-mss_clamp.start",
+		"/etc/lime-init.d/15-mss_clamp.start",
 		"iptables -t mangle -A FORWARD -p tcp -o bmx+ -m tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu")
 
 end
