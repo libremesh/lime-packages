f210d6d6d44160f265ed6c13a3a493ebfd889928
Frank95 <franciscojim95@gmail.com>
2014-06-11 21:33:33 +0200

    lime-hwd-usbradio: initial work on usb radio detection

---
+++ packages/lime-hwd-usbradio/src/hotplug-hook.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+burstStopFile="/tmp/lime-hotplug-usbradio-burst-check"
+
+[ -e "${burstStopFile}" ] ||
+{
+	lua_hotplug_args="hotplug_hook_args = { action='${ACTION}', devicename='${DEVICENAME}', devname='${DEVNAME}', devpath='${DEVPATH}', product='${PRODUCT}', type='${TYPE}', interface='${INTERFACE}' }"
+	date > "${burstStopFile}"
+	((sleep 2s ; lua -e"${lua_hotplug_args}" /usr/lib/lua/lime/hwd/usbradio.lua ; rm -rf "${burstStopFile}")&)
+}
