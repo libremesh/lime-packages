b232cbeb24bdb1035e1e55bce607a5e24d8eae24
Frank95 <franciscojim95@gmail.com>
2014-06-18 19:51:27 +0200

    lime-system: Hardware detection lime module implement clean and configure

--- packages/lime-system/files/usr/lib/lua/lime/hardware_detection.lua
+++ packages/lime-system/files/usr/lib/lua/lime/hardware_detection.lua
@@ -1,11 +1,27 @@
 #!/usr/bin/lua
 
+local fs = require("nixio.fs")
+local utils = require("lime.utils")
+
+
 hardware_detection = {}
 
 function hardware_detection.clean()
+	for hwd_module_path in fs.glob("/usr/lib/lua/lime/hwd/*.lua") do
+		local module_name = "lime.hwd." .. fs.basename(hwd_module_path):sub(1,-5)
+		if utils.isModuleAvailable(module_name) then
+			require(module_name).clean()
+		end
+	end
 end
 
 function hardware_detection.configure()
+	for hwd_module_path in fs.glob("/usr/lib/lua/lime/hwd/*.lua") do
+		local module_name = "lime.hwd." .. fs.basename(hwd_module_path):sub(1,-5)
+		if utils.isModuleAvailable(module_name) then
+			require(module_name).detect_hardware()
+		end
+	end
 end
 
 
