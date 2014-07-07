e37dec32c143d71a0bc31629e0dbc1ba2a29ce2e
Gui Iribarren <gui@altermundi.net>
2014-07-07 05:35:55 -0300

    rework dnsmasq integration: use dhcp-hostsfile as database, which gets reread on HUP, avoiding a full daemon restart

--- packages/dnsmasq-lease-share/src/dnsmasq-lease-share.lua
+++ packages/dnsmasq-lease-share/src/dnsmasq-lease-share.lua
@@ -3,6 +3,7 @@
 --[[
 
 Copyright (C) 2013 Gioacchino Mazzurco <gio@eigenlab.org>
+Copyright (C) 2014 Gui Iribarren <gui@altermundi.net>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -19,20 +20,14 @@ along with this file. If not, see <http://www.gnu.org/licenses/>.
 
 -- PLEASE USE TAB NOT SPACE JUST FOR INDENTATION
 
-
---! dhcp lease file lines format
---! <Time of lease expiry, in epoch time (seconds since 1970)> <Client MAC Address> <Client IP> <Client unqualified hostname if provided, * if not provided> <Client-ID, if known. The client-ID is used as the computer's unique-ID in preference to the MAC address, if it's available>
-
---! root at OpenWrt:~# cat /var/dhcp.leases
---! 946689575 00:00:00:00:00:05 192.168.1.155 wdt 01:00:00:00:00:00:05
---! 946689351 00:0f:b0:3a:b5:0b 192.168.1.208 colinux *
---! 946689493 02:0f:b0:3a:b5:0b 192.168.1.199 * 01:02:0f:b0:3a:b5:0b
+--! dhcp-hostsfile line format
+--! [<hwaddr>][,id:<client_id>|*][,set:<tag>][,<ipaddr>][,<hostname>][,<lease_time>][,ignore]
 
 require("uci");
 
-local local_lease_file = "/tmp/dhcp.leases_dnsmasq-lease-share"
-local alfred_shared_lease_num = "65"
-local own_lease_lifetime = "600" -- in seconds
+local local_lease_file = "/tmp/dhcp.hosts_local"
+local dnsmasq_dhcp_hostsfile = "/tmp/dhcp.hosts_remote"
+local alfred_shared_lease_num = "66"
 
 --! Tell alfred local dhcp lease changed
 function update_alfred()
@@ -41,6 +36,11 @@ function update_alfred()
 	stdin:write(lease_file:read("*all"));
 	lease_file:close();
 	stdin:close();
+end
+
+--! Tell dnsmasq to reread dhcp-hostsfile
+function reload_dnsmasq()
+	os.execute("killall -HUP dnsmasq 2>/dev/null")
 end
 
 function get_hostname()
@@ -57,8 +57,8 @@ function get_if_mac(ifname)
 	return ret_string;
 end
 
-function add_lease(lease_expiration, client_mac, client_ip, client_hostname, client_id)
-	local lease_line = lease_expiration .. " " .. client_mac .. " " .. client_ip .. " " .. client_hostname .. " " .. client_id .. "\n";
+function add_lease(client_mac, client_ip, client_hostname, client_id)
+	local lease_line = client_mac .. ",id:" .. client_id .. "," .. client_ip .. "," .. client_hostname .. "\n";
 
 	local lease_file = io.open(local_lease_file, "a");
 	lease_file:write(lease_line);
@@ -84,13 +84,12 @@ local command = arg[1];
 local client_mac = arg[2];
 local client_ip = arg[3];
 local client_hostname;
-if (arg[4] and (arg[4]:len() > 0)) then client_hostname = arg[4] else client_hostname = "*" end;
-local lease_expiration = os.getenv("DNSMASQ_LEASE_EXPIRES");
+if (arg[4] and (arg[4]:len() > 0)) then client_hostname = arg[4] else client_hostname = "" end;
 local client_id = os.getenv("DNSMASQ_CLIENT_ID");
 if ((not client_id) or (client_id:len() <= 0)) then client_id = client_mac end;
 
 if command == "add" then
-	add_lease(lease_expiration, client_mac, client_ip, client_hostname, client_id)
+	add_lease(client_mac, client_ip, client_hostname, client_id)
 	update_alfred()
 
 elseif command == "del" then
@@ -99,10 +98,12 @@ elseif command == "del" then
 
 elseif command == "old" then
 	del_lease(client_mac)
-	add_lease(lease_expiration, client_mac, client_ip, client_hostname, client_id)
+	add_lease(client_mac, client_ip, client_hostname, client_id)
 	update_alfred()
 
-elseif command == "init" then
+elseif command == nil then
+--! ran from cron as an alfred facter,
+--! publish our own host details
 	local uci_conf = uci.cursor();
 
 	local own_hostname = get_hostname();
@@ -110,9 +111,10 @@ elseif command == "init" then
 	local own_mac = get_if_mac("br-lan");
 
 	del_lease(own_mac)
-	add_lease(os.time()+own_lease_lifetime, own_mac, own_ipv4, own_hostname, own_mac);
+	add_lease(own_mac, own_ipv4, own_hostname, own_mac);
 	update_alfred()
 
+--! and populate dhcp-hostsfile with incoming data
 	local stdout = io.popen("alfred -r " .. alfred_shared_lease_num,"r");
 	local raw_output = stdout:read("*a");
 	stdout:close();
@@ -126,9 +128,15 @@ elseif command == "init" then
 
 	for _, row in ipairs(json_output) do
 		local node_mac, value = unpack(row)
+		table.insert(lease_table, "# Node ".. node_mac .. "\n")
 		table.insert(lease_table, value:gsub("\x0a", "\n") .. "\n")
 	end
 
-	print(table.concat(lease_table));
+	local hostsfile = io.open(dnsmasq_dhcp_hostsfile, "w");
+	if hostsfile then
+		hostsfile:write(table.concat(lease_table));
+		hostsfile:close();
+	end
+	reload_dnsmasq()
 
 end
