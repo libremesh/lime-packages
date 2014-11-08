111adc6d44783e101a23f052822eb094cebd253c
Gioacchino Mazzurco <gio@eigenlab.org>
2014-11-08 22:06:25 +0100

    Removed workaround for iussue #32
    Conflicts:
    	packages/lime-proto-batadv/src/batadv.lua

--- packages/lime-proto-batadv/src/batadv.lua
+++ packages/lime-proto-batadv/src/batadv.lua
@@ -15,32 +15,17 @@ function batadv.setup_interface(ifname, args)
 	local mtu = 1532
 	if ifname:match("^eth") then mtu = 1496 end
 
-	-- Unless a specific integer is passed, parse network_id (%N1) template
-	-- and use that number + 16 to get a vlanId between 16 and 271 for batadv
-	-- (to avoid overlapping with other protocols)
+	--! Unless a specific integer is passed, parse network_id (%N1) template
+	--! and use that number + 16 to get a vlanId between 16 and 271 for batadv
+	--! (to avoid overlapping with other protocols)
 	if not tonumber(vlanId) then vlanId = 16 + utils.applyNetTemplate10(vlanId) end
 
 	local owrtInterfaceName, _, owrtDeviceName = network.createVlanIface(ifname, vlanId, nameSuffix, vlanProto)
 
 	local uci = libuci:cursor()
-
 	uci:set("network", owrtDeviceName, "mtu", mtu)
-
-	-- BEGIN
-	-- Workaround to http://www.libre-mesh.org/issues/32
-	-- We create a new macaddress for ethernet vlan interface
-	-- We change the 7nt bit to 1 to give it locally administered meaning
-	-- Then use it as the new mac address prefix "02"
-	if ifname:match("^eth") then
-		local vlanMacAddr = network.get_mac(ifname:gsub("%..*", ""))
-		vlanMacAddr[1] = "02"
-		uci:set("network", owrtDeviceName, "macaddr", table.concat(vlanMacAddr, ":"))
-	end
-	--- END
-
 	uci:set("network", owrtInterfaceName, "proto", "batadv")
 	uci:set("network", owrtInterfaceName, "mesh", "bat0")
-
 	uci:save("network")
 end
 
