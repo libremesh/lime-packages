87381f5c892d9ab2d79f02c81534259e1f1bc4a7
Gioacchino Mazzurco <gio@eigenlab.org>
2014-12-26 19:22:57 +0100

    lime-hwd-ground-routing: there is no software way to associate switchX to ethY so read those informations from the configurations lime-system: support passing informations about the device from scanning to protos

--- packages/lime-hwd-ground-routing/src/ground_routing.lua
+++ packages/lime-hwd-ground-routing/src/ground_routing.lua
@@ -25,30 +25,58 @@ end
 
 function ground_routing.detect_hardware()
 	function parse_gr(section)
+		local link_name = section[".name"]
+		local net_dev = section["net_dev"]
 		local vlan = section["vlan"]
-		local physdev = section["physdev"]
-		local secname = ground_routing.sectionNamePrefix..section[".name"].."_"..physdev.."_"..vlan
 
-		local switch_ports = section["switch_ports"]
-		if switch_ports then
-			local ports = ""
-			for _,p in pairs(switch_ports) do ports = ports..p.."t " end
+		local uci = libuci:cursor()
 
-			local uci = libuci:cursor()
-			uci:set("network", secname, "switch_vlan")
-			uci:set("network", secname, "device", physdev)
-			uci:set("network", secname, "vlan", vlan)
-			uci:set("network", secname, "ports", ports)
-			uci:save("network")
-		else
-			local uci = libuci:cursor()
-			uci:set("network", secname, "device")
-			uci:set("network", secname, "name", physdev.."."..vlan)
-			uci:set("network", secname, "ifname", physdev)
-			uci:set("network", secname, "type", "8021q")
-			uci:set("network", secname, "vid", vlan)
-			uci:save("network")
+		function create_8021q_dev(vlan_p)
+			local dev_secname = ground_routing.sectionNamePrefix..link_name.."_"..net_dev.."_"..vlan_p
+			uci:set("network", dev_secname, "device")
+			uci:set("network", dev_secname, "name", net_dev.."."..vlan_p)
+			uci:set("network", dev_secname, "ifname", net_dev)
+			uci:set("network", dev_secname, "type", "8021q")
+			uci:set("network", dev_secname, "vid", vlan_p)
 		end
+
+		local switch_dev = section["switch_dev"]
+		if switch_dev then
+			local switch_cpu_port = section["switch_cpu_port"]
+			function tag_cpu_port(section)
+				if (section["device"] ~= switch_dev) then return end
+
+				local patterns = { "^"..switch_cpu_port.." ", " "..switch_cpu_port.."$", " "..switch_cpu_port.." "  }
+				local substits = { switch_cpu_port.."t ",     " "..switch_cpu_port.."t", " "..switch_cpu_port.."t " }
+				local matchCount = 0
+				local m = 0
+				for i,p in pairs(patterns) do
+					section["ports"], m = section["ports"]:gsub(p, substits[i])
+					matchCount = matchCount + m
+				end
+
+				if (matchCount > 0) then
+					create_8021q_dev(section["vlan"])
+					uci:set("network", section[".name"], "ports", section["ports"])
+				end
+			end
+
+			uci:foreach("network", "switch_vlan", tag_cpu_port)
+
+
+			local sw_secname = ground_routing.sectionNamePrefix..link_name.."_sw_"..switch_dev.."_"..vlan
+			local ports = switch_cpu_port.."t"
+			for _,p in pairs(section["switch_ports"]) do ports = ports.." "..p.."t" end
+
+			uci:set("network", sw_secname, "switch_vlan")
+			uci:set("network", sw_secname, "device", switch_dev)
+			uci:set("network", sw_secname, "vlan", vlan)
+			uci:set("network", sw_secname, "ports", ports)
+		end
+
+		create_8021q_dev(vlan)
+
+		uci:save("network")
 	end
 
 	config.foreach("hwd_gr", parse_gr)
--- packages/lime-system/files/usr/lib/lua/lime/proto/lan.lua
+++ packages/lime-system/files/usr/lib/lua/lime/proto/lan.lua
@@ -7,17 +7,19 @@ local libuci = require("uci")
 
 function lan.configure(args)
 	local ipv4, ipv6 = network.primary_address()
-	
+
 	local uci = libuci:cursor()
 	uci:set("network", "lan", "ip6addr", ipv6:string())
 	uci:set("network", "lan", "ipaddr", ipv4:host():string())
 	uci:set("network", "lan", "netmask", ipv4:mask():string())
 	uci:set("network", "lan", "proto", "static")
 	uci:set("network", "lan", "mtu", "1500")
+	uci:delete("network", "lan", "ifname")
 	uci:save("network")
 end
 
 function lan.setup_interface(ifname, args)
+	if args and args["nobridge"] then return end
 	if ifname:match("^wlan") then return end
 	if ifname:match(network.protoVlanSeparator.."%d+$") then return end
 
