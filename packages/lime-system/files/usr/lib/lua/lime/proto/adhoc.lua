aa51809183e0066f0b1324d5fd69d03fcacbc536
Gui Iribarren <gui@altermundi.net>
2015-06-11 19:48:27 -0300

    lime-system: construct ifnames using '_' as protoVlanSeparator and '-' as wifiModeSeparator

--- packages/lime-system/files/usr/lib/lua/lime/proto/adhoc.lua
+++ packages/lime-system/files/usr/lib/lua/lime/proto/adhoc.lua
@@ -5,11 +5,13 @@ local adhoc = {}
 function adhoc.configure(args) end
 
 function adhoc.setup_interface(ifname, args)
-	if ifname:match("^wlan%d_adhoc") then
+	if ifname:match("^wlan%d.adhoc") then
 		local libuci = require "uci"
 		local uci = libuci:cursor()
 
-		local networkInterfaceName = network.limeIfNamePrefix..ifname
+		--! sanitize passed ifname for constructing uci section name
+		--! because only alphanumeric and underscores are allowed
+		local networkInterfaceName = network.limeIfNamePrefix..ifname:gsub("[^%w_]", "_")
 
 		uci:set("network", networkInterfaceName, "interface")
 		uci:set("network", networkInterfaceName, "proto", "none")
