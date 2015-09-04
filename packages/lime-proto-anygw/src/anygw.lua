9ea660cc424082380f015406169980064281289a
Gio <gio@eigenlab.org>
2015-09-04 11:16:46 +0200

    lime-proto-anygw: implement bgp_conf(...)

--- packages/lime-proto-anygw/src/anygw.lua
+++ packages/lime-proto-anygw/src/anygw.lua
@@ -97,4 +97,13 @@ end
 
 function anygw.setup_interface(ifname, args) end
 
+function anygw.bgp_conf(templateVarsIPv4, templateVarsIPv6)
+	local base_conf = [[
+protocol direct {
+	interface "anygw";
+}
+]]
+	return base_conf
+end
+
 return anygw
