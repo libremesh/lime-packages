5add2b1cf9986800e109d9cb130d56845bc693d4
Gui Iribarren <gui@altermundi.net>
2015-09-09 04:18:28 -0300

    lime-proto-anygw: hotfix /etc/dnsmasq.d/ conf which broke global resolution

--- packages/lime-proto-anygw/src/anygw.lua
+++ packages/lime-proto-anygw/src/anygw.lua
@@ -64,16 +64,13 @@ function anygw.configure(args)
 	)
 
 	local content = { }
-	table.insert(content, "interface=anygw")
-	table.insert(content, "except-interface=br-lan")
+	table.insert(content, "no-dhcp-interface=br-lan")
 	fs.writefile("/etc/dnsmasq.d/lime-proto-anygw-00-interfaces.conf", table.concat(content, "\n").."\n")
 
 	content = { }
 	table.insert(content, "dhcp-range=tag:anygw,"..anygw_ipv4:add(1):host():string()..","..ipv4:maxhost():string())
 	table.insert(content, "dhcp-option=tag:anygw,option:router,"..anygw_ipv4:host():string())
 	table.insert(content, "dhcp-option=tag:anygw,option:dns-server,"..anygw_ipv4:host():string())
-	table.insert(content, "dhcp-option=tag:anygw,option:domain-name,lan")
-	table.insert(content, "dhcp-option=tag:anygw,option:domain-search,lan")
 	table.insert(content, "dhcp-option-force=tag:anygw,option:mtu,1350")
 	table.insert(content, "dhcp-broadcast=tag:anygw")
 	table.insert(content, "address=/anygw/"..anygw_ipv4:host():string())
