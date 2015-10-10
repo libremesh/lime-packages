8bcf0f2f060c16c765e8a24b2b52e50764c8d685
Gui Iribarren <gui@altermundi.net>
2015-10-10 01:01:14 -0300

    lime-proto-anygw: add thisnode.info alias domain, equivalent to anygw

--- packages/lime-proto-anygw/src/anygw.lua
+++ packages/lime-proto-anygw/src/anygw.lua
@@ -74,6 +74,7 @@ function anygw.configure(args)
 	table.insert(content, "dhcp-option-force=tag:anygw,option:mtu,1350")
 	table.insert(content, "dhcp-broadcast=tag:anygw")
 	table.insert(content, "address=/anygw/"..anygw_ipv4:host():string())
+	table.insert(content, "address=/thisnode.info/"..anygw_ipv4:host():string())
 	fs.writefile("/etc/dnsmasq.d/lime-proto-anygw-10-ipv4.conf", table.concat(content, "\n").."\n")
 
 	content = { }
@@ -82,6 +83,7 @@ function anygw.configure(args)
 	table.insert(content, "dhcp-option=tag:anygw,option6:domain-search,lan")
 	table.insert(content, "dhcp-option=tag:anygw,option6:dns-server,"..anygw_ipv6:host():string())
 	table.insert(content, "address=/anygw/"..anygw_ipv6:host():string())
+	table.insert(content, "address=/thisnode.info/"..anygw_ipv6:host():string())
 	fs.writefile("/etc/dnsmasq.d/lime-proto-anygw-20-ipv6.conf", table.concat(content, "\n").."\n")
 
 	io.popen("/etc/init.d/dnsmasq enable || true"):close()
