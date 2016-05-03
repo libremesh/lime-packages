45802420c1f67ac500ec532afbe3d8628da21a7d
gabri94 <gabrihacker@gmail.com>
2016-05-04 00:40:21 +0200

    finished olsrd2 plugin with loopback if and default conf file

--- packages/lime-proto-olsr2/src/olsr2.lua
+++ packages/lime-proto-olsr2/src/olsr2.lua
@@ -6,7 +6,7 @@ local fs = require("nixio.fs")
 local libuci = require("uci")
 local wireless = require("lime.wireless")
 local utils = require("lime.utils")
-
+local ip = require("luci.ip")
 olsr2 = {}
 
 olsr2.configured = false
@@ -17,47 +17,40 @@ function olsr2.configure(args)
 
 	local uci = libuci:cursor()
 	local ipv4, ipv6 = network.primary_address()
-
+	local origInterfaceName = network.limeIfNamePrefix.."olsr_originator_lo"
 
 	fs.writefile("/etc/config/olsrd2", "")
-
 	uci:set("olsrd2", "lime", "global")
 	uci:set("olsrd2", "lime", "failfast", "no")
 	uci:set("olsrd2", "lime", "pidfile", "/var/run/olsrd2.pid")
 	uci:set("olsrd2", "lime", "lockfile", "/var/lock/olsrd2")
-
-
-
 	uci:set("olsrd2", "limelog", "log")
 	uci:set("olsrd2", "limejson", "syslog", "true")
 	uci:set("olsrd2", "limejson", "info", "all")
-
 	uci:set("olsrd2", "limetelnet", "telnet")
 	uci:set("olsrd2", "limetelnet", "port", "2009")
-
+	uci:set("olsrd2", owrtInterfaceName, "interface")
+	uci:set("olsrd2", owrtInterfaceName, "ifname", "loopback")
 	uci:save("olsrd2")
 
-	uci:set("network", "loopback", "ipaddr", ipv4)
-	uci:set("network", "loopback", "ipaddr", ipv6)
-
-
+	uci set("network" origInterfaceName, "interface")
+	uci set("network" origInterfaceName, "ifname", "@loopback")
+	uci set("network" origInterfaceName, "proto", "static")
+	uci set("network" origInterfaceName, "ipaddr", ipv4:host():string())
+	uci set("network" origInterfaceName, "netmask", "255.255.255.255")
+	uci set("network" origInterfaceName, "ip6addr", ipv6:host():string().."/128")
+	uci:save("network")
 end
 
 function olsr2.setup_interface(ifname, args)
 	if not args["specific"] then
 		if ifname:match("^wlan%d+.ap") then return end
 	end
-	local vlanId = args[2] or 14
+	local vlanId = args[2] or 16
 	local vlanProto = args[3] or "8021ad"
 	local nameSuffix = args[4] or "_olsr"
-	--!local ipPrefixTemplate = args[5] or "169.254.%M5.%M6/16"
 	local owrtInterfaceName, linux802adIfName, owrtDeviceName = network.createVlanIface(ifname, vlanId, nameSuffix, vlanProto)
-	local macAddr = network.get_mac(utils.split(ifname, ".")[1])
-	local ipAddr = ip.IPv4(utils.applyMacTemplate10(ipPrefixTemplate, macAddr))
-
 	local uci = libuci:cursor()
-	uci:set("network", owrtInterfaceName, "proto", "static")
-	uci:save("network")
 
 	uci:set("olsrd2", owrtInterfaceName, "interface")
 	uci:set("olsrd2", owrtInterfaceName, "interface", owrtInterfaceName)
