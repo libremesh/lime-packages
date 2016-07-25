9ae8dae78bc089f72ff5fd5a6b46b160d79b1485
Micha Stöcker <micha@stoecker.me>
2016-07-25 12:17:31 +0200

    fix for #57

--- packages/lime-system/files/etc/lime/rc.d/apply-eb-ip-rules-if-not-fw.sh
+++ packages/lime-system/files/etc/lime/rc.d/apply-eb-ip-rules-if-not-fw.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 [ ! -e /etc/rc.d/S??firewall ] && {
   echo "$0: Firewall is not enabled. Executing firewall.user scripts."
-  sh /etc/firewall.user.d/*
+  for file in /etc/firewall.user.d/*
+    do
+      sh "$file"
+    done
 }
