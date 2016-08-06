libremesh/lime-packages

commit 8a817ffca277e2856943e623d09282c5fd0e1c1c

network.primary_address check if generated IPv4 is valid Fix: #34

packages/lime-system/files/usr/lib/lua/lime/network.lua
@@ -60,9 +60,18 @@ function network.primary_address(offset)
     ipv4_template = network.generate_host(ip.IPv4(ipv4_template), hexsuffix)
     ipv6_template = network.generate_host(ip.IPv6(ipv6_template), hexsuffix)
 
-    ipv4_template:prefix(tonumber(ipv4_maskbits))
+	ipv4_template:prefix(tonumber(ipv4_maskbits))
+	local m4 = ipv4_template:mihost()
+	--! Generated address is network address like 192.0.2.0/24 ?
+	local invalid = ipv4_template:equal(m4:network())
+	--! Generated address is the one reserved for anygw like 192.0.2.1/24 ?
+	invalid = invalid or ipv4_template:equal(m4)
+	--! Generated address is the broadcast address like 192.0.2.255/24 ?
+	invalid = invalid or ipv4_template:equal(m4:broadcast())
+	if invalid then ipv4_template = ipv4_template:maxhost() end
+
     ipv6_template:prefix(tonumber(ipv6_maskbits))
-    
+
     return ipv4_template, ipv6_template
 end
 

commit 9544dfb87db5164077d0e73551f583767dbb2005

Fix typo introduced in commit 8a817ff

packages/lime-system/files/usr/lib/lua/lime/network.lua
@@ -61,7 +61,7 @@ function network.primary_address(offset)
     ipv6_template = network.generate_host(ip.IPv6(ipv6_template), hexsuffix)
 
 	ipv4_template:prefix(tonumber(ipv4_maskbits))
-	local m4 = ipv4_template:mihost()
+	local m4 = ipv4_template:minhost()
 	--! Generated address is network address like 192.0.2.0/24 ?
 	local invalid = ipv4_template:equal(m4:network())
 	--! Generated address is the one reserved for anygw like 192.0.2.1/24 ?