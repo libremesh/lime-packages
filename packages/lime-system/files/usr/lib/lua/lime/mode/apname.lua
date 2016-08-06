a17e6170e838a29dcd0b65bde1fa0906b0898333
Pau Escrich <p4u@dabax.net>
2016-08-07 00:37:06 +0200

    Add WiFi mode "apname": Adds the possibility of having a second AP interface per device with another SSID (Hostname by default). So user can choose between WiFi roaming or WiFi static by choosing the right SSID.
    Signed-off-by: Pau Escrich <p4u@dabax.net>

--- packages/lime-system/files/usr/lib/lua/lime/mode/apname.lua
+++ packages/lime-system/files/usr/lib/lua/lime/mode/apname.lua
@@ -7,7 +7,7 @@ ap.wifi_mode="ap"
 function ap.setup_radio(radio, args)
 --!	checks("table", "?table")
 
-	return wireless.createBaseWirelessIface(radio, ap.wifi_mode, nil, { network = "lan" })
+	return wireless.createBaseWirelessIface(radio, ap.wifi_mode, "name", { network = "lan" })
 end
 
 return ap
