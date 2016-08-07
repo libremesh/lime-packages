bb3606b4c609f3e89ce64b48ad77ada9136a0bbc
Pau Escrich <p4u@dabax.net>
2016-08-07 18:24:44 +0200

    Rewrite libre-mesh LuCi Simple Config
    Signed-off-by: Pau Escrich <p4u@dabax.net>

--- packages/lime-webui/src/model/essentials.lua
+++ packages/lime-webui/src/model/essentials.lua
@@ -1,5 +1,5 @@
 --[[
-    Copyright (C) 2011 Fundacio Privada per a la Xarxa Oberta, Lliure i Neutral guifi.net
+    Copyright (C) 2016 Libre-Mesh.org
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -18,36 +18,91 @@
     The full GNU General Public License is included in this distribution in
     the file called "COPYING".
 --]]
-m = Map("lime", "Libre-Mesh")
+
+-- Libre-Mesh.org config
+
+require("luci.sys")
+local lime = Map("lime", "Libre-Mesh")
+local config = require("lime.config")
 
 -- Create sections
-local system = m:section(NamedSection, "system", "lime","System","System")
-system.addremove = true
-
-local network = m:section(NamedSection, "network", "lime","Network","Network")
-network.addremove = true
-
-local wifi = m:section(TypedSection, "wifi","WiFi","WiFi")
-wifi.addremove = true
+local system = lime:section(NamedSection, "system", "lime","System","System")
+system.addremove = false
+local network = lime:section(NamedSection, "network", "lime","Network","Network")
+network.addremove = false
+local wifi = lime:section(NamedSection, "wifi","WiFi","WiFi")
+wifi.addremove = false
 
 -- hostname
-system:option(Value,"hostname",translate("Hostname"),translate("Name for this node"))
+local hostname = system:option(Value,"hostname",translate("Hostname"),
+  translate("Name for this node"))
+hostname.default = config.get("system","hostname")
 
 -- network
-network:option(Value,"main_ipv4_address",translate("Main IPv4"),translate("The main IPv4 configured for this node, with slash notation (for ex. 1.2.3.4/24)"))
-network:option(Value,"main_ipv6_address",translate("Main IPv6"),translate("The main IPv6 configured for this node, with slash notation (for ex. 2001:db8::1/64)"))
+local ipv4 = network:option(Value,"main_ipv4_address",translate("Main IPv4"),
+  translate("The main IPv4 configured for this node, with slash notation (for ex. 1.2.3.4/24)"))
+ipv4.default = config.get("network","main_ipv4_address")
+ipv4.optional = true
+local ipv6 = network:option(Value,"main_ipv6_address",translate("Main IPv6"),
+  translate("The main IPv6 configured for this node, with slash notation (for ex. 2001:db8::1/64)"))
+ipv6.default = config.get("network","main_ipv6_address")
+ipv6.optional = true
 
 -- wifi
-wifi:option(Value,"public_essid",translate("Public SSID"),translate("The SSID (WiFi network name) used for this node"))
-wifi:option(Value,"channel",translate("Channel"),translate("Channel used for this interface"))
-wifi:option(Value,"ap_essid",translate("AP SSID"),translate("The SSID (WiFi network name) used for the access point devices"))
-wifi:option(Value,"adhoc_essid",translate("Mesh SSID"),translate("The SSID (WiFi network name) used for the ad-hoc device"))
-wifi:option(Value,"adhoc_bssid",translate("Mesh BSSID"),translate("The BSSID (WiFi network identifier) used for the ad-hoc network"))
+local ap_ssid = wifi:option(Value,"ap_ssid",translate("Network SSID"),
+  translate("Access Point ESSID (usually name of your network)"))
+ap_ssid.default = config.get("wifi","ap_ssid")
+ap_ssid.optional = true
+local ch_2 = wifi:option(Value,"channel_2ghz",translate("Channel 2.4 GHz"),
+  translate("Default channel used for 2.4 GHz radios"))
+ch_2.default = config.get("wifi","channel_2ghz")
+ch_2.optional = true
+local ch_5 = wifi:option(Value,"channel_5ghz",translate("Channel 5 GHz"),
+  translate("Default channel used for 5 GHz radios"))
+ch_5.default = config.get("wifi","channel_5ghz")
+ch_5.optional = true
+local country = wifi:option(Value,"country",translate("Country code"),
+  translate("Country code used for WiFi radios"))
+country.default = config.get("wifi","country")
+country.optional = true
+local bssid = wifi:option(Value,"adhoc_bssid",translate("Mesh BSSID"),
+  translate("The BSSID (WiFi network identifier) used for the mesh network"))
+bssid.default = config.get("wifi","adhoc_bssid")
+bssid.optional = true
 
 -- commit
-function m.on_commit(self,map)
-	luci.sys.call('true')
+function lime.on_commit(self,map)
+  luci.sys.call('(/usr/bin/lime-config > /tmp/lime-config.log 2>&1 && reload_config) &')
 end
 
-return m
+-- LibreMap.org config
 
+limap = Map("libremap", "Libre-Map")
+
+-- main settings
+local settings = limap:section(NamedSection, "settings", "libremap",translate("General"),translate("General"))
+settings.addremove = false
+local com = settings:option(Value,"community",translate("Community name"),
+  translate("The name of your network community"))
+com.default = config.get("wifi","ap_ssid")
+
+-- location
+local loc = limap:section(NamedSection, "location", "plugin",translate("Location"),translate("Map location"))
+loc:option(Value,"latitude",translate("Latitude"),
+  translate("Latitude (this can be also configured in location tab)"))
+loc:option(Value,"longitude",translate("Longitude"),
+  translate("Longitude (this can be also configured in location tab)"))
+
+  -- contact
+local contact = limap:section(NamedSection, "contact", "plugin",translate("Contact"),translate("Contact information"))
+contact:option(Value,"name",translate("Name"),
+  translate("Your name or alias (not required)"))
+contact:option(Value,"email",translate("Email"),
+  translate("Your email (not required)"))
+
+-- commit
+function limap.on_commit(self,map)
+	luci.sys.call('/usr/sbin/libremap-agent &')
+end
+
+return lime,limap
