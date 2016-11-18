563c731da0d99d836c82694f73d3e8674d261b11
Gui Iribarren <gui@altermundi.net>
2016-11-18 14:34:24 +0100

    lime-system: fix ieee80211s proto, correctly construct ifnames
    This is essentially the same as aa51809183e0066f0b1324d5fd69d03fcacbc536
    "lime-system: construct ifnames using '_' as protoVlanSeparator and '-' as wifiModeSeparator"
    in that commit, i forgot to update ieee80211s.lua in addition to adhoc.lua

--- packages/lime-system/files/usr/lib/lua/lime/proto/ieee80211s.lua
+++ packages/lime-system/files/usr/lib/lua/lime/proto/ieee80211s.lua
@@ -11,11 +11,13 @@ function ieee80211s.configure(args)
 end
 
 function ieee80211s.setup_interface(ifname, args)
-	if ifname:match("^wlan%d+_"..ieee80211s_mode.wifi_mode) then
+	if ifname:match("^wlan%d+."..ieee80211s_mode.wifi_mode) then
 		local libuci = require "uci"
 		local uci = libuci:cursor()
 
-		local networkInterfaceName = network.limeIfNamePrefix..ifname
+		--! sanitize passed ifname for constructing uci section name
+		--! because only alphanumeric and underscores are allowed
+		local networkInterfaceName = network.limeIfNamePrefix..ifname:gsub("[^%w_]", "_")
 
 		uci:set("network", networkInterfaceName, "interface")
 		uci:set("network", networkInterfaceName, "proto", "none")
