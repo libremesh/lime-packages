9a0b4c10a6afc112c63261925ebec9bb9609260c
Nicolas Pace <nicopace@gmail.com>
2017-03-17 23:05:44 -0300

    Sanitizes hostname.

--- packages/lime-system/files/usr/lib/lua/lime/utils.lua
+++ packages/lime-system/files/usr/lib/lua/lime/utils.lua
@@ -137,4 +137,13 @@ function utils.expandVars(s, ...)
 	return s
 end
 
+function utils.sanitize_hostname(hostname)
+	hostname = hostname:gsub(' ', '-')
+	hostname = hostname:gsub('[^-a-zA-Z0-9]', '')
+	hostname = hostname:gsub('^-*', '')
+	hostname = hostname:gsub('-*$', '')
+	hostname = hostname:sub(1, 32)
+	return hostname
+end
+
 return utils
