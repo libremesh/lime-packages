6ded289f2db783a5895f3e214f6f957092b5c4f5
Gio <gio@eigenlab.org>
2017-05-18 11:21:44 +0200

    Fix: WiFi client mode #47
    Trying to hijack a "by default proto" in a wifi mode that could be used
    for other protos breack the architecture, at current time there is no
    staisfactory proposal on how to _suggest_ a default proto without
    breaking the architecture.
    The cleaner approach seems to have the wifi client mode working the same
    as all other modes.

--- packages/lime-system/files/usr/lib/lua/lime/mode/client.lua
+++ packages/lime-system/files/usr/lib/lua/lime/mode/client.lua
@@ -6,8 +6,6 @@ client.wifi_mode="sta"
 
 function client.setup_radio(radio, args)
 --!	checks("table", "?table")
-
-  args["network"] = "wan"
 	return wireless.createBaseWirelessIface(radio, client.wifi_mode, nil, args)
 end
 
