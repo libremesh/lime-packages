aca2df10e31c22ec676485c5710b6fd9a1b89dbc
Nicolas Pace <nicopace@gmail.com>
2017-08-06 13:42:51 +0300

    Changes to branch number.

--- .ci/after_success.sh
+++ .ci/after_success.sh
@@ -5,8 +5,8 @@ chmod 400 $TRAVIS_BUILD_DIR/.ci/travisci_rsa
 openssl aes-256-cbc -K $encrypted_055f76aafa25_key -iv $encrypted_055f76aafa25_iv -in $TRAVIS_BUILD_DIR/.ci/announce_link_in_PR.enc -out $TRAVIS_BUILD_DIR/.ci/announce_link_in_PR -d
 chmod +x $TRAVIS_BUILD_DIR/.ci/announce_link_in_PR
 if [ $TRAVIS_BUILD_DIR ]; then
-    mv $TRAVIS_BUILD_DIR/sdk/output $TRAVIS_BUILD_DIR/sdk/$TRAVIS_BRANCH
-    rsync -r -v -e "ssh -i $TRAVIS_BUILD_DIR/.ci/travisci_rsa -o UserKnownHostsFile=$TRAVIS_BUILD_DIR/.ci/known_hosts" $TRAVIS_BUILD_DIR/sdk/$TRAVIS_BRANCH ci@repo.libremesh.org:/var/www/ci/
+    mv $TRAVIS_BUILD_DIR/sdk/output $TRAVIS_BUILD_DIR/sdk/$TRAVIS_PULL_REQUEST
+    rsync -r -v -e "ssh -i $TRAVIS_BUILD_DIR/.ci/travisci_rsa -o UserKnownHostsFile=$TRAVIS_BUILD_DIR/.ci/known_hosts" $TRAVIS_BUILD_DIR/sdk/$TRAVIS_PULL_REQUEST ci@repo.libremesh.org:/var/www/ci/
     $TRAVIS_BUILD_DIR/.ci/announce_link_in_PR
 else
     echo "not a pull request branch, skip copying binaries."
