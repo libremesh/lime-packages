1e1d587662646b6b623215d3b634917d832529d0
p4u <p4u@dabax.net>
2018-01-09 16:12:03 +0100

    use config.get_bool() for bmx6_publish_ownip option
    Signed-off-by: p4u <p4u@dabax.net>

--- packages/lime-proto-bmx6/src/bmx6.lua
+++ packages/lime-proto-bmx6/src/bmx6.lua
@@ -32,14 +32,14 @@ function bmx6.configure(args)
 	uci:set(bmx6.f, "main", "tun6Address", ipv6:string())
 
 	-- If publish own IP enabled, configure tunIn
-	local pub_own_ip = config.get("network", "bmx6_publish_ownip")
-	if (pub_own_ip == '1' or pub_own_ip == 'true') then
+	local pub_own_ip = config.get_bool("network", "bmx6_publish_ownip", false)
+	if (pub_own_ip) then
 		uci:set(bmx6.f, "myIP4", "tunIn")
 		uci:set(bmx6.f, "myIP4", "tunIn", "myIP4")
-		uci:set(bmx6.f, "myIP4", "network", utils.split(ipv4:string(),'/')[1]..'/32')
+		uci:set(bmx6.f, "myIP4", "network", ipv4:host():string()..'/32')
 		uci:set(bmx6.f, "myIP6", "tunIn")
 		uci:set(bmx6.f, "myIP6", "tunIn", "myIP6")
-		uci:set(bmx6.f, "myIP6", "network", utils.split(ipv6:string(),'/')[1]..'/128')
+		uci:set(bmx6.f, "myIP6", "network", ipv6:host():string()..'/128')
 	end
 
 	-- Enable bmx6 uci config plugin
