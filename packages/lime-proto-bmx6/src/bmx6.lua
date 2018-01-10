77f374da67cb80298cebc65b1add64a63b725b1d
Gioacchino Mazzurco <gio@eigenlab.org>
2018-01-10 13:37:22 +0100

    proto-bmx6: Use function from libraries avoid reinventing
    Use config_bool to get boolean value from the configurations
    Use ip:host() to extract host ip instead of string split

--- packages/lime-proto-bmx6/src/bmx6.lua
+++ packages/lime-proto-bmx6/src/bmx6.lua
@@ -32,14 +32,13 @@ function bmx6.configure(args)
 	uci:set(bmx6.f, "main", "tun6Address", ipv6:string())
 
 	-- If publish own IP enabled, configure tunIn
-	local pub_own_ip = config.get("network", "bmx6_publish_ownip")
-	if (pub_own_ip == '1' or pub_own_ip == 'true') then
-		uci:set(bmx6.f, "myIP4", "tunIn")
-		uci:set(bmx6.f, "myIP4", "tunIn", "myIP4")
-		uci:set(bmx6.f, "myIP4", "network", utils.split(ipv4:string(),'/')[1]..'/32')
-		uci:set(bmx6.f, "myIP6", "tunIn")
-		uci:set(bmx6.f, "myIP6", "tunIn", "myIP6")
-		uci:set(bmx6.f, "myIP6", "network", utils.split(ipv6:string(),'/')[1]..'/128')
+	if config.get_bool("network", "bmx6_publish_ownip") then
+		uci:set(bmx6.f, "ownIPv4", "tunIn")
+		uci:set(bmx6.f, "ownIPv4", "tunIn", "ownIPv4")
+		uci:set(bmx6.f, "ownIPv4", "network", ipv4:host():string()..'/32')
+		uci:set(bmx6.f, "ownIPv6", "tunIn")
+		uci:set(bmx6.f, "ownIPv6", "tunIn", "ownIPv6")
+		uci:set(bmx6.f, "ownIPv6", "network", ipv6:host():string()..'/128')
 	end
 
 	-- Enable bmx6 uci config plugin
