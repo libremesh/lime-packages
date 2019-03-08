10254e134b55803d0bd411d8241e9f38269f43c5
Gioacchino Mazzurco <gio@eigenlab.org>
2019-03-08 13:58:49 -0300

    shared-state multiple fixes

--- packages/shared-state-dnsmasq_leases/files/usr/bin/dnsmasq-lease-share.sh
+++ packages/shared-state-dnsmasq_leases/files/usr/bin/dnsmasq-lease-share.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-((shared-state-publish_dnsmasq_leases $@; shared-state sync dnsmasq-leases)&)
+((shared-state-publish_dnsmasq_leases $@; shared-state sync dnsmasq-leases; shared-state sync dnsmasq-hosts)&)
