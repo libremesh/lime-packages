108d976d2c2221f55fa6abcd0c5d8e16b4273309
Nicolas Pace <nicopace@gmail.com>
2019-06-20 16:18:13 -0300

    Removes event-based sync as it creates a loop that drains router resources.

--- packages/shared-state-dnsmasq_leases/files/usr/bin/dnsmasq-lease-share.sh
+++ packages/shared-state-dnsmasq_leases/files/usr/bin/dnsmasq-lease-share.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-((shared-state-publish_dnsmasq_leases $@; shared-state sync dnsmasq-leases; shared-state sync dnsmasq-hosts) &>/dev/null &)
+((shared-state-publish_dnsmasq_leases $@) &>/dev/null &)
