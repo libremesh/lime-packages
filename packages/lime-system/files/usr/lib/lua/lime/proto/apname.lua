a3a3a7e69f515b991302ddff9f74f5fc5006457a
Ilario Gelmetti <iochesonome@gmail.com>
2019-07-25 13:51:45 +0200

    create proto-apname, similar to proto-ap but specifying explicit 'name' suffix on top of apname_mode.wifi_mode which is just 'ap'

---
+++ packages/lime-system/files/usr/lib/lua/lime/proto/apname.lua
@@ -0,0 +1,31 @@
+#!/usr/bin/lua
+
+local apname_mode = require("lime.mode.apname")
+
+local apname = {}
+
+apname.configured = false
+
+function apname.configure(args)
+	apname.configured = true
+end
+
+function apname.setup_interface(ifname, args)
+	if ifname:match("^wlan%d+."..apname_mode.wifi_mode.."name$") then
+		local libuci = require "uci"
+		local uci = libuci:cursor()
+
+		--! sanitize passed ifname for constructing uci section name
+		--! because only alphanumeric and underscores are allowed
+		local networkInterfaceName = network.limeIfNamePrefix..ifname:gsub("[^%w_]", "_")
+
+		uci:set("network", networkInterfaceName, "interface")
+		uci:set("network", networkInterfaceName, "proto", "none")
+		uci:set("network", networkInterfaceName, "mtu", "1536")
+		uci:set("network", networkInterfaceName, "auto", "1")
+
+		uci:save("network")
+	end
+end
+
+return apname
