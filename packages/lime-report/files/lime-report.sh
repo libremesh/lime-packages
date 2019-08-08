1c5e2733d267f3d19082b7560a6461363e669ffe
Ilario Gelmetti <iochesonome@gmail.com>
2019-08-08 15:13:00 +0200

    lime-report changed options and added more content

--- packages/lime-report/files/lime-report.sh
+++ packages/lime-report/files/lime-report.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-export PASTE_SERVICE="http://paste.libremesh.org"
-export PASTE_PORT="8080"
-
 paste_file() {
-    echo -n "\n### FILE $1"
+    echo -ne "\n### FILE $1"
     [ -e "$1" ] && (
-        echo "\n" &&
+        echo -e "\n" &&
         cat "$1" | grep -v key | grep -v pass
     ) || echo -e " NOT FOUND\n"
 }
@@ -16,23 +13,41 @@ paste_cmd() {
     eval $@ 2>&1
 }
 
-generate() {
+header() {
+    paste_cmd echo $HOSTNAME
+    paste_cmd date \'+%Y-%m-%d %H:%M:%S\'
+    paste_cmd uptime
+}
+
+generate_deviceinfo() {
+    paste_file /etc/board.json
+    paste_file /proc/cpuinfo
+    paste_file /etc/lime_release
+    paste_file /etc/openwrt_release
+    paste_file /etc/openwrt_version
+}
+
+generate_config() {
     paste_file /etc/config/lime
     paste_file /etc/config/lime-defaults
+    paste_file /etc/config/lime-defaults-factory
     paste_file /etc/config/network
     paste_file /etc/config/wireless
+}
+
+generate_status() {
     paste_cmd dmesg | tail -c 10
     paste_cmd batctl if
     paste_cmd batctl o
     paste_cmd bmx6 -c show=status show=interfaces show=links show=originators show=tunnels
     paste_cmd bmx7 -c show=status show=interfaces show=links show=originators show=tunnels
+    paste_cmd "echo dump | nc ::1 30003"
     paste_cmd free
     paste_cmd ps
     paste_cmd ip address show
     paste_cmd ip route show
     paste_cmd brctl show
     paste_cmd ip link show
-    paste_file /proc/cpuinfo
     paste_cmd df
     paste_cmd logread -l 20
     paste_cmd iw dev wlan0-mesh station dump
@@ -40,18 +55,25 @@ generate() {
     paste_cmd iw dev wlan0-mesh mpath dump
     paste_cmd iw dev wlan1-mesh mpath dump
     paste_cmd iwinfo
+    paste_cmd wifi status
+    paste_cmd swconfig dev switch0 show
 }
 
-[ "$1" = "" ] || [ "$1" = "-h" ] && {
-    echo "-h    for help" && \
-    echo "-p    paste to $PASTE_SERVICE" && \
-    echo "-f    store in /tmp/<date_time>.txt" && \
-    echo "-o    print in stdout" 
-} 
-[ "$1" = "-o" ] && generate
-[ "$1" = "-p" ] && generate | lime-paste
-[ "$1" = "-f" ] && { 
-    REPORT_FILE="/tmp/lime-paste_$(date '+%Y-%m-%d_%H%M%S').txt"
-    generate > "$REPORT_FILE"
-    echo "stored in $REPORT_FILE"
+generate_all() {
+    generate_deviceinfo
+    generate_config
+    generate_status
 }
+
+[ "$1" = "--help" ] || [ "$1" = "-h" ] && {
+    echo "Usage: $0 [OPTION]" && \
+    echo "Prints information useful for debugging a LibreMesh network" && \
+    echo "-h    for help" && \
+    echo "-d    print only device info" && \
+    echo "-c    print only main configuration" && \
+    echo "-s    print only current status"
+}
+[ "$1" = "" ] && header && generate_all
+[ "$1" = "-d" ] && header && generate_deviceinfo
+[ "$1" = "-c" ] && header && generate_config
+[ "$1" = "-s" ] && header && generate_status
