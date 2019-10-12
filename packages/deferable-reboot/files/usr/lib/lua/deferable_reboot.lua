8425f9fa35a940a3d589bafc3b3d3767b444a65a
Santiago Piccinini <spiccinini@altermundi.net>
2019-10-12 14:24:26 -0300

    add deferable-reboot package
    This package provides a deferable reboot mechanism. The system
    will be rebooted when the uptime of the system reach a specified amount
    of seconds and if /tmp/deferable-reboot.defer file does not contain a
    number that is greater than the current uptime.
    This way if anyone wants to postpone the reboot, the way is to store the
    next checkpoint in seconds. For example if the system must not reboot from
    now and up to 120 seconds in the future then something like this can be donde
    awk '{print $1 + 120}' /proc/uptime > /tmp/deferable-reboot.defer

---
+++ packages/deferable-reboot/files/usr/lib/lua/deferable_reboot.lua
@@ -0,0 +1,55 @@
+local utils = require "lime.utils"
+local config = require "lime.config"
+
+local defreboot = {}
+
+defreboot.DEFAULT_REBOOT_UPTIME = 60*60*7
+defreboot.SLEEP_BEFORE_REBOOT_S = 30
+
+defreboot.POSTPONE_FILE_PATH = '/tmp/deferable-reboot.defer'
+
+function defreboot.config(min_uptime)
+	if min_uptime == nil then
+		min_uptime = tonumber(config.get("system", "deferable_reboot_uptime",
+										 defreboot.DEFAULT_REBOOT_UPTIME))
+	end
+	assert(type(min_uptime) == "number", "min_uptime must be a number")
+	defreboot.min_uptime = min_uptime
+end
+
+function defreboot.should_reboot()
+	local uptime_s = utils.uptime_s()
+	local postpone_until_s = defreboot.read_postpone_file()
+	local min_uptime = defreboot.min_uptime
+
+	if (postpone_until_s ~= nil) and (postpone_until_s > min_uptime) then
+		min_uptime = postpone_until_s
+	end
+	return uptime_s > min_uptime
+end
+
+function defreboot.postpone_util_s(uptime)
+	assert(type(uptime) == "number", "uptime must be a number")
+	local f = io.open(defreboot.POSTPONE_FILE_PATH, 'w')
+	f:write(tostring(uptime))
+	f:close()
+end
+
+
+--! use this function to postpone the reboot, also the following command can be used
+--! replacing SECONDS: # awk '{print $1 + SECONDS}' /proc/uptime > /tmp/deferable-reboot.defer
+function defreboot.read_postpone_file()
+	local f = io.open(defreboot.POSTPONE_FILE_PATH)
+	if f ~= nil then
+		return tonumber(f:read("*l"))
+	end
+	return nil
+end
+
+function defreboot.reboot()
+	--! give time to sysupgrade to kill us
+	nixio.nanosleep(defreboot.SLEEP_BEFORE_REBOOT_S)
+	os.execute("reboot")
+end
+
+return defreboot
