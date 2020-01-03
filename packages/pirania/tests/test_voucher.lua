929af018dbc90e7f25d8d0683b8a8dd23eaa1ffb
luandro <luandro@gmail.com>
2020-01-03 16:16:35 -0300

    added renew_many_vouchers function with tests

--- packages/pirania/tests/test_voucher.lua
+++ packages/pirania/tests/test_voucher.lua
@@ -17,6 +17,7 @@ local upload = 10
 local download = 10
 local amountofmacsallowed = 1
 local renewDate = 0
+local renewManyDate = 15796472566666
 
 function formatData (db)
     data = {}
@@ -29,8 +30,6 @@ function formatData (db)
     end
     return data
 end
-
-print(voucher)
 
 describe('Pirania tests #voucher', function()
     it ('create voucher', function ()
@@ -47,6 +46,18 @@ describe('Pirania tests #voucher', function()
         local output = logic.update_voucher_date(voucher, renewDate, db)
         assert.is.equal(true, output.success)
         assert.is.equal(renewDate, db.data[index][3])
+        utils.from_table_to_csv(dbFile, formatData(db))
+    end)
+    it('renew many vouchers', function()
+        local db = dba.load(dbFile)
+        local vouchersToUpdate = {}
+        for _, voucher in pairs (db.data) do
+            vouchersToUpdate[_] = voucher[2]
+        end
+        local db = dba.load(dbFile)
+        local output = logic.update_many_vouchers_date(vouchersToUpdate, renewManyDate, db)
+        assert.is.equal(true, output.success)
+        assert.is.equal(#vouchersToUpdate, output.updated)
         utils.from_table_to_csv(dbFile, formatData(db))
     end)
 end)
