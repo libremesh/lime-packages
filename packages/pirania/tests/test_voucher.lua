0d34faf7b4f3d16d8e0001d7ae7ebb40837d6929
Santiago Piccinini <spiccinini@altermundi.net>
2020-01-22 09:19:20 -0300

    pirania: make tests to not depend of each other
    each unit tests must be an independent unit. Following this rule allows
    to run a subgroup of the tests and more important if one test fails then
    the following fill fail if they are dependent and this make difficult
    to find the problem.

--- packages/pirania/tests/test_voucher.lua
+++ packages/pirania/tests/test_voucher.lua
@@ -12,8 +12,8 @@ local hostname = fs.readfile("/proc/sys/kernel/hostname"):gsub("\n","")
 math.randomseed(os.time())
 
 local note = 'Marcos Android'
-local voucher = tostring(math.random())
-local epoc = utils.dateNow() + 600
+local voucherId = tostring(math.random())
+local expirationEpocMs = utils.dateNow() + 600
 local upload = 10
 local download = 10
 local amountofmacsallowed = 1
@@ -36,6 +36,14 @@ function formatData (db)
     return data
 end
 
+local function create_test_voucher()
+    local db = dba.load(dbFile)
+    local output = { logic.add_voucher(db, memberKey, voucherId, expirationEpocMs, upload, download, amountofmacsallowed)}
+    utils.from_table_to_csv(dbFile, formatData(db))
+end
+
+local snapshot
+
 describe('Pirania tests #voucher', function()
     it ('format member voucher key', function ()
         memberKey = utils.format_voucher_key(note, 'member')
@@ -47,46 +55,51 @@ describe('Pirania tests #voucher', function()
     end)
     it ('create voucher', function ()
         local db = dba.load(dbFile)
-        local output = { logic.add_voucher(db, memberKey, voucher, epoc, upload, download, amountofmacsallowed)}
+        local output = { logic.add_voucher(db, memberKey, voucherId, expirationEpocMs, upload, download, amountofmacsallowed)}
         assert.is.equal(upload, output[2])
         assert.is.equal(download, output[3])
         assert.is.equal(amountofmacsallowed, tonumber(output[4]))
-        utils.from_table_to_csv(dbFile, formatData(db))
     end)
     it ('create same voucher', function ()
+        create_test_voucher()
         local db = dba.load(dbFile)
-        local output = { logic.add_voucher(db, memberKey, voucher, epoc, upload, download, amountofmacsallowed)}
+        local output = { logic.add_voucher(db, memberKey, voucherId, expirationEpocMs, upload, download, amountofmacsallowed)}
         assert.is.equal('0', output[2])
-        utils.from_table_to_csv(dbFile, formatData(db))
     end)
     it ('authenticate voucher', function ()
+        create_test_voucher()
         local db = dba.load(dbFile)
-        local res = logic.auth_voucher(db, mac, voucher)
+        local res = logic.auth_voucher(db, mac, voucherId)
         assert.is.equal(true, res.success)
         assert.is.equal(tostring(amountofmacsallowed), res.limit[4])
-        utils.from_table_to_csv(dbFile, formatData(db))
     end)
-    it ('authenticate same voucher', function ()
+    it ('authenticate same voucher fails', function ()
+        create_test_voucher()
         local db = dba.load(dbFile)
-        local res = logic.auth_voucher(db, mac, voucher)
+
+        local res = logic.auth_voucher(db, mac, voucherId)
+        assert.is.equal(true, res.success)
+
+        local res = logic.auth_voucher(db, mac, voucherId)
         assert.is.equal(false, res.success)
-        utils.from_table_to_csv(dbFile, formatData(db))
     end)
     it ('show active vouchers', function ()
+        create_test_voucher()
         local db = dba.load(dbFile)
         local output = logic.show_active_vouchers(db)
         assert.is_not_nil(output.visitors)
         assert.is.equal(#db.data, output.members)
     end)
     it('renew voucher', function()
+        create_test_voucher()
         local db = dba.load(dbFile)
         local index = #db.data
-        local output = logic.update_voucher_date(voucher, renewDate, db)
+        local output = logic.update_voucher_date(voucherId, renewDate, db)
         assert.is.equal(true, output.success)
         assert.is.equal(renewDate, db.data[index][3])
-        utils.from_table_to_csv(dbFile, formatData(db))
     end)
     it('renew many vouchers', function()
+        create_test_voucher()
         local db = dba.load(dbFile)
         local vouchersToUpdate = {}
         for _, voucher in pairs (db.data) do
@@ -96,13 +109,24 @@ describe('Pirania tests #voucher', function()
         local output = logic.update_many_vouchers_date(vouchersToUpdate, renewManyDate, db)
         assert.is.equal(true, output.success)
         assert.is.equal(#vouchersToUpdate, output.updated)
-        utils.from_table_to_csv(dbFile, formatData(db))
     end)
     it('list all vouchers', function()
+        create_test_voucher()
         local db = dba.load(dbFile)
         local output = logic.show_vouchers(db)
-        assert.is.equal(voucher, output[#output].voucher)
+        assert.is.equal(voucherId, output[#output].voucher)
         assert.is.equal('member', output[#output].type)
         assert.is.equal(hostname, output[#output].node)
+    end)
+
+    before_each('', function()
+        local p = io.popen("rm -f " .. dbFile)
+        p:read('*all')
+        p:close()
+        snapshot = assert:snapshot()
+    end)
+
+    after_each('', function()
+        snapshot:revert()
     end)
 end)
