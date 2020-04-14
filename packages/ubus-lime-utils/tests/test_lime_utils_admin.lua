1535920e8ee90f1b755dfaf3df08b52a3e72e3ee
Santiago Piccinini <spiccinini@altermundi.net>
2020-04-14 13:31:57 -0300

    ubus-lime-utils: add lime_utils_admin tests

---
+++ packages/ubus-lime-utils/tests/test_lime_utils_admin.lua
@@ -0,0 +1,31 @@
+local utils = require "lime.utils"
+local test_utils = require "tests.utils"
+local config = require 'lime.config'
+
+local test_file_name = "packages/ubus-lime-utils/files/usr/libexec/rpcd/lime-utils-admin"
+local ubus_lime_utils = test_utils.load_lua_file_as_function(test_file_name)
+
+local rpcd_call = test_utils.rpcd_call
+local uci
+
+describe('ubus-lime-utils-admin tests #ubuslimeutilsadmin', function()
+    it('test list methods', function()
+        local response  = rpcd_call(ubus_lime_utils, {'list'})
+        assert.is.equal(nil, response.set_root_password.no_params)
+    end)
+
+    it('test set_root_password', function()
+        stub(utils, "set_password", function (user, pass) return pass end)
+        local response  = rpcd_call(ubus_lime_utils, {'call', 'set_root_password'},
+                                    '{"password": "foo"}')
+        assert.is.equal("ok", response.status)
+    end)
+
+    before_each('', function()
+        uci = test_utils.setup_test_uci()
+    end)
+
+    after_each('', function()
+        test_utils.teardown_test_uci(uci)
+    end)
+end)
