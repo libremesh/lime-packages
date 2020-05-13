b2d007734cf93778ecdea465ce2b1df0c0e50bd6
Santiago Piccinini <spiccinini@altermundi.net>
2020-05-13 01:38:09 -0300

    proto-batadv: add backward compat and tests
    Add support for batman pre 2019.0-2 config files used in OpenWrt
    up to 18.06.

--- packages/lime-proto-batadv/files/usr/lib/lua/lime/proto/batadv.lua
+++ packages/lime-proto-batadv/files/usr/lib/lua/lime/proto/batadv.lua
@@ -9,43 +9,63 @@ local config = require("lime.config")
 batadv = {}
 
 batadv.configured = false
+batadv.old_cfg_api = false
+batadv.type_option = 'master'
+batadv.ifc_proto = 'batadv_hardif'
+
+function batadv.detect_old_cfg_api()
+    return not fs.lstat("/lib/netifd/proto/batadv_hardif.sh")
+end
 
 function batadv.configure(args)
 	if batadv.configured then return end
 	batadv.configured = true
 
+    --! Detect batman config API until 2019.0-2/OpenWrt 18.06.x
+    local cfg_file = 'network'
+	if batadv.detect_old_cfg_api() then
+		batadv.old_cfg_api = true
+		cfg_file = 'batman-adv'
+		batadv.ifc_proto = 'batadv'
+		batadv.type_option = 'mesh'
+    end
+
 	local uci = config.get_uci_cursor()
 
-	uci:set("network", "bat0", "interface")
-	uci:set("network", "bat0", "proto", "batadv")
-	uci:set("network", "bat0", "interface")
-	uci:set("network", "bat0", "bridge_loop_avoidance", "1")
-	uci:set("network", "bat0", "multicast_mode", "0")
+	if not batadv.old_cfg_api then
+		uci:set(cfg_file, "bat0", "interface")
+		uci:set(cfg_file, "bat0", "proto", "batadv")
+    else
+		uci:set(cfg_file, "bat0", "mesh")
+	end
+
+	uci:set(cfg_file, "bat0", "bridge_loop_avoidance", "1")
+	uci:set(cfg_file, "bat0", "multicast_mode", "0")
 
 	-- if anygw enabled disable DAT that doesn't play well with it
 	-- and set gw_mode=client everywhere. Since there's no gw_mode=server, this makes bat0 never forward requests
 	-- so a rogue DHCP server doesn't affect whole network (DHCP requests are always answered locally)
 	for _,proto in pairs(config.get("network", "protocols")) do
 		if proto == "anygw" then
-			uci:set("network", "bat0", "distributed_arp_table", "0")
-			uci:set("network", "bat0", "gw_mode", "client")
+			uci:set(cfg_file, "bat0", "distributed_arp_table", "0")
+			uci:set(cfg_file, "bat0", "gw_mode", "client")
 		end
 	end
-
+	uci:save(cfg_file)
 	lan.setup_interface("bat0", nil)
 
 	--! Avoid dmesg flooding caused by BLA. Create a dummy0 interface with
 	--! custom MAC, as dummy0 is created very soon on boot it is added as
 	--! first and then main interface to batman so BLA messages are sent
-	--! from that MAC avoiding generating warning like:  
+	--! from that MAC avoiding generating warning like:
 	--! br-lan: received packet on bat0 with own address as source address
 	local owrtInterfaceName = network.limeIfNamePrefix.."batadv_dummy_if"
 	local dummyMac = network.primary_mac(); dummyMac[1] = "aa"
 	uci:set("network", owrtInterfaceName, "interface")
 	uci:set("network", owrtInterfaceName, "ifname", "dummy0")
 	uci:set("network", owrtInterfaceName, "macaddr", table.concat(dummyMac, ":"))
-	uci:set("network", owrtInterfaceName, "proto", "batadv_hardif")
-	uci:set("network", owrtInterfaceName, "master", "bat0")
+	uci:set("network", owrtInterfaceName, "proto", batadv.ifc_proto)
+	uci:set("network", owrtInterfaceName, batadv.type_option, "bat0")
 	uci:save("network")
 
 	-- enable alfred on bat0 if installed
@@ -76,8 +96,8 @@ function batadv.setup_interface(ifname, args)
 
 	local uci = config.get_uci_cursor()
 	uci:set("network", owrtDeviceName, "mtu", mtu)
-	uci:set("network", owrtInterfaceName, "proto", "batadv_hardif")
-	uci:set("network", owrtInterfaceName, "master", "bat0")
+	uci:set("network", owrtInterfaceName, "proto", batadv.ifc_proto)
+	uci:set("network", owrtInterfaceName, batadv.type_option, "bat0")
 	uci:save("network")
 end
 
