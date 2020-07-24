aaf589e5d3dcbfa45f345864e8c8a800acf22279
Santiago Piccinini <spiccinini@altermundi.net>
2020-07-24 20:47:55 -0300

    safe-upgrade: make symbols not exposed as local

--- packages/safe-upgrade/tests/test_safe-upgade.lua
+++ packages/safe-upgrade/tests/test_safe-upgade.lua
@@ -1,7 +1,7 @@
 local su = require 'safe-upgrade'
 local utils = require 'lime.utils'
 local test_utils = require 'tests.utils'
-
+local json = require 'luci.jsonc'
 
 describe('safe-upgrade tests #safeupgrade', function()
 
@@ -17,6 +17,29 @@ describe('safe-upgrade tests #safeupgrade', function()
         assert.is_false(su.is_current_board_supported())
         su.get_current_device:revert()
         su.get_supported_devices:revert()
+    end)
+
+    it('test is_firmware_valid()', function()
+        local meta_lros = '{  "supported_devices":["librerouter-v1"] }'
+        local meta_master = '{  "supported_devices":["librerouter,librerouter-v1"]}'
+        local meta_v2 = '{  "supported_devices":["other,librerouter-v2"]}'
+        local meta_other = '{  "supported_devices":["other"]}'
+        stub(su, 'get_current_device', function () return 'librerouter-v1' end)
+        assert.is_true(su.is_firmware_valid(json.parse(meta_lros)))
+        assert.is_true(su.is_firmware_valid(json.parse(meta_master)))
+        assert.is_false(su.is_firmware_valid(json.parse(meta_v2)))
+        assert.is_false(su.is_firmware_valid(json.parse(meta_other)))
+        assert.is_false(su.is_firmware_valid(nil))
+        assert.is_false(su.is_firmware_valid({}))
+
+        stub(su, 'get_current_device', function () return 'librerouter-v2' end)
+        assert.is_false(su.is_firmware_valid(json.parse(meta_lros)))
+        assert.is_false(su.is_firmware_valid(json.parse(meta_master)))
+        assert.is_false(su.is_firmware_valid(json.parse(meta_v2)))
+        assert.is_false(su.is_firmware_valid(json.parse(meta_other)))
+        assert.is_false(su.is_firmware_valid(nil))
+        assert.is_false(su.is_firmware_valid({}))
+        su.get_current_device:revert()
     end)
 
 end)
