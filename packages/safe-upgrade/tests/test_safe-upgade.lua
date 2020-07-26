568e344e2784df29b974d240c75e5cf043f39923
Santiago Piccinini <spiccinini@altermundi.net>
2020-07-26 15:42:50 -0300

    safe-upgrade: more testing and refactor using utils

--- packages/safe-upgrade/tests/test_safe-upgade.lua
+++ packages/safe-upgrade/tests/test_safe-upgade.lua
@@ -3,7 +3,58 @@ local utils = require 'lime.utils'
 local test_utils = require 'tests.utils'
 local json = require 'luci.jsonc'
 
+local test_dir = nil
+
 describe('safe-upgrade tests #safeupgrade', function()
+    it('test get_partitions()', function()
+        stub(su, 'get_uboot_env',
+            function(part_name)
+                if part_name == su.STABLE_PARTITION_NAME then
+                    return 1
+                elseif part_name == su.TESTING_PARTITION_NAME then
+                    return 0
+                else
+                    assert.is_true(false)
+                end
+            end)
+        local mtd_file = test_dir .. 'mtd'
+        local mtd = [[dev:    size   erasesize  name
+mtd0: 00040000 00010000 "u-boot"
+mtd1: 00010000 00010000 "u-boot-env"
+mtd2: 007d0000 00010000 "firmware"
+mtd3: 00160000 00010000 "kernel"
+mtd4: 00670000 00010000 "rootfs"
+mtd5: 00480000 00010000 "rootfs_data"
+mtd6: 007d0000 00010000 "fw2"
+mtd7: 00010000 00010000 "ART"
+]]
+        utils.write_file(mtd_file, mtd)
+        su._mtd_partitions_desc = mtd_file
+        local partitions = su.get_partitions()
+        assert.are.equal(1, partitions.current)
+        assert.are.equal(2, partitions.other)
+        assert.are.equal(1, partitions.stable)
+        assert.are.equal(0, partitions.testing)
+
+        local mtd = [[dev:    size   erasesize  name
+mtd0: 00040000 00010000 "u-boot"
+mtd1: 00010000 00010000 "u-boot-env"
+mtd2: 007c0000 00010000 "fw1"
+mtd3: 007c0000 00010000 "firmware"
+mtd4: 00160000 00010000 "kernel"
+mtd5: 00660000 00010000 "rootfs"
+mtd6: 00310000 00010000 "rootfs_data"
+mtd7: 00020000 00010000 "res"
+mtd8: 00010000 00010000 "ART"
+]]
+        utils.write_file(mtd_file, mtd)
+        su._mtd_partitions_desc = mtd_file
+        local partitions = su.get_partitions()
+        assert.are.equal(2, partitions.current)
+        assert.are.equal(1, partitions.other)
+        assert.are.equal(1, partitions.stable)
+        assert.are.equal(0, partitions.testing)
+    end)
 
     it('test is_current_board_supported()', function()
         stub(su, 'get_supported_devices', function () return {'librerouter-v1', 'librerouter-v2'} end)
