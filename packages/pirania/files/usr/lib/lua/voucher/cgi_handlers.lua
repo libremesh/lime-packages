7a3609980dd955df756393316180dd9e8620691d
Santiago Piccinini <spiccinini@altermundi.net>
2020-08-03 19:12:52 -0300

    pirania: refactor and rename auth cgi-handlers and add tests

--- packages/pirania/files/usr/lib/lua/voucher/cgi_handlers.lua
+++ packages/pirania/files/usr/lib/lua/voucher/cgi_handlers.lua
@@ -3,12 +3,18 @@ local utils = require('voucher.utils')
 
 handlers = {}
 
-function handlers.handle_voucher()
+local TESTING_URL_INFO = '/info'
+local TESTING_URL_FAIL = '/fail'
+local TESTING_URL_AUTHENTICATED = '/authenticated'
+
+
+function handlers.preactivate_voucher()
     local uci_cursor = require('uci').cursor()
 
-    local url_authenticated = uci_cursor:get("pirania", "base_config", "url_authenticated") or 'authenticated'
-    local url_fail = uci_cursor:get("pirania", "base_config", "url_fail") or 'fail'
-    local url_info = uci_cursor:get("pirania", "base_config", "url_info") or 'info'
+    --! using defaults for testing as uci environment is not available
+    local url_authenticated = uci_cursor:get("pirania", "base_config", "url_authenticated") or TESTING_URL_AUTHENTICATED
+    local url_fail = uci_cursor:get("pirania", "base_config", "url_fail") or TESTING_URL_FAIL
+    local url_info = uci_cursor:get("pirania", "base_config", "url_info") or TESTING_URL_INFO
 
     vouchera.init()
 
@@ -21,8 +27,7 @@ function handlers.handle_voucher()
         url = url_authenticated
     else
         local output
-        local query_string = os.getenv("QUERY_STRING")
-        local params = utils.urldecode_params(query_string)
+        local params = utils.urldecode_params(os.getenv("QUERY_STRING"))
         local code = params['voucher']
         local prevUrl = params['prev']
         --! if client does not have javascript then activate right away without going to the INFO portal
@@ -46,5 +51,36 @@ function handlers.handle_voucher()
     return url
 end
 
+function handlers.activate_voucher()
+    local uci_cursor = require('uci').cursor()
+
+    local url_authenticated = uci_cursor:get("pirania", "base_config", "url_authenticated") or TESTING_URL_AUTHENTICATED
+    local url_fail = uci_cursor:get("pirania", "base_config", "url_fail") or TESTING_URL_FAIL
+
+    vouchera.init()
+    local client_data = utils.getIpv4AndMac(os.getenv('REMOTE_ADDR'))
+    local client_is_authorized = vouchera.is_mac_authorized(client_data.mac)
+
+    local params = utils.urldecode_params(os.getenv("QUERY_STRING"))
+    local code = params['voucher']
+    local prevUrl = params['prev']
+
+    if client_is_authorized then
+        return url_authenticated
+    end
+
+    local url = url_fail
+
+    if code and client_data.mac then
+        if vouchera.activate(code, client_data.mac) then
+            if prevUrl ~= nil then
+                url = prevUrl
+            else
+                url = url_authenticated
+            end
+        end
+    end
+    return url
+end
 
 return handlers
