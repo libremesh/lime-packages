5d04715bafce176301f96d8b541239ebccf6cbac
Ilario Gelmetti <iochesonome@gmail.com>
2020-10-19 16:33:00 +0200

    print lime-app notes at SSH login

---
+++ packages/ubus-lime-utils/files/etc/profile.d/system_notes.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo ""
+echo " === System Notes ====================================="
+[ -f /etc/banner.notes ] && cat /etc/banner.notes
+echo ""
+echo " == edit this note via lime-app or /etc/banner.notes =="
+echo ""
