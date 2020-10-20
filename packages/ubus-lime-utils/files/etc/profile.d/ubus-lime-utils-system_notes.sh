bd8bbb5e22c80572e62eb0a4c54dd8b4487797e5
Ilario Gelmetti <iochesonome@gmail.com>
2020-10-20 17:53:59 +0200

    renamed system_notes.sh to avoid conflicts with lime-webui

---
+++ packages/ubus-lime-utils/files/etc/profile.d/ubus-lime-utils-system_notes.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo ""
+echo " === System Notes ================================================="
+[ -f /etc/banner.notes ] && cat /etc/banner.notes
+echo ""
+echo " = edit via http://thisnode.info/app/#/notes or /etc/banner.notes ="
+echo ""
