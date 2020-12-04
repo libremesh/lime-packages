9e0f446790d0b61f4565f431878c370515ea5643
Santiago Piccinini <spiccinini@altermundi.net>
2020-12-04 17:44:37 -0300

    pirania: fix hooks with empty directory

--- packages/pirania/files/usr/lib/lua/voucher/hooks.lua
+++ packages/pirania/files/usr/lib/lua/voucher/hooks.lua
@@ -5,11 +5,12 @@ local fs = require("nixio.fs")
 
 local hooks = function(action)
     local hookPath = config.hooksDir..action..'/'
-    local files = fs.dir(hookPath) or pairs({})
-    for file in files do
-        os.execute("(( sh "..hookPath..file.." 0<&- &>/dev/null &) &)")
+    local files = fs.dir(hookPath)
+    if files then
+        for file in files do
+            os.execute("(( sh "..hookPath..file.." 0<&- &>/dev/null &) &)")
+        end
     end
-
 end
 
 if debug.getinfo(2).name == nil then
