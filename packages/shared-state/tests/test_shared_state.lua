750e3252989691d2205d3b35a8d56593359c6dd7
Santiago Piccinini <spiccinini@altermundi.net>
2021-01-18 22:30:52 -0300

    shared-state: add first simple test

---
+++ packages/shared-state/tests/test_shared_state.lua
@@ -0,0 +1,25 @@
+local utils = require 'lime.utils'
+local test_utils = require 'tests.utils'
+local SharedState = require 'shared-state'
+
+local test_dir
+
+describe('LiMe Utils tests #sharedstate', function()
+    it('test load a new and empty db', function()
+        local sharedState = SharedState('foo')
+        sharedState.setDataDir(test_dir)
+        sharedState.lock()
+        sharedState.load()
+        sharedState.unlock()
+        assert.equal("[]", sharedState.toJsonString())
+    end)
+
+    before_each('', function()
+        test_dir = test_utils.setup_test_dir()
+    end)
+
+    after_each('', function()
+        test_utils.teardown_test_dir()
+    end)
+
+end)
