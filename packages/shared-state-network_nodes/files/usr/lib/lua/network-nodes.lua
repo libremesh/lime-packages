55fe592d32b7abd93877de010af878966a501600
Santiago Piccinini <spiccinini@altermundi.net>
2021-03-30 23:20:22 -0300

    shared-state-network_nodes: many improvements

--- packages/shared-state-network_nodes/files/usr/lib/lua/network-nodes.lua
+++ packages/shared-state-network_nodes/files/usr/lib/lua/network-nodes.lua
@@ -1,23 +1,91 @@
 local shared_state = require("shared-state")
+local utils = require("lime.utils")
+
 local network_nodes = {}
 
-function network_nodes.get_nodes()
-    local sharedState = shared_state.SharedStatePersistent:new("network_nodes")
-    local result = {}
-    for key, value in pairs(sharedState:get()) do
-        local status = value.data and "connected" or "disconnected"
-        table.insert(result, { hostname = key, status = status})
+function network_nodes.node(hostname, member, fw_version, board)
+    return {hostname=hostname, member=member, fw_version=fw_version, board=board}
+end
+
+function network_nodes.serialize_for_network_nodes(node)
+    return {hostname=node.hostname, member=node.member, fw_version=node.fw_version, board=node.board}
+end
+
+function network_nodes.deserialize_from_network_nodes(data)
+    return network_nodes.node(data.hostname, data.member, data.fw_version, data.board)
+end
+
+function network_nodes._nodes_from_db(db)
+    local nodes = {}
+    for hostname, value in pairs(db:get()) do
+        nodes[hostname] = network_nodes.deserialize_from_network_nodes(value.data)
     end
-    return result
+    return nodes
+end
+
+function network_nodes.create_node()
+    local hostname = utils.hostname()
+    local fw_version = utils.release_info()['DISTRIB_RELEASE']
+    local board = utils.current_board()
+    local member = true
+    local node = network_nodes.node(hostname, member, fw_version, board)
+    node.status = "recently_connected"
+    return node
+end
+
+function network_nodes.publish()
+    local node = network_nodes.create_node()
+    local data = {
+        [node.hostname] = network_nodes.serialize_for_network_nodes(node)
+    }
+    network_nodes_db = shared_state.SharedStateMultiWriter:new("network_nodes")
+    network_nodes_db:insert(data)
+end
+
+function network_nodes.as_csv()
+    local nodes = network_nodes.get_nodes()
+    local tmpl = "%-30s %-40s %-30s %-20s\n"
+    local out = string.format(tmpl, "hostname", "board", "fw_version", "status")
+    for _, node in pairs(nodes) do
+        if node.member then
+            out = out .. string.format(tmpl, node.hostname, node.board, node.fw_version, node.status)
+        end
+    end
+    return out
+end
+
+
+function network_nodes.get_nodes()
+    local network_nodes_db = shared_state.SharedStateMultiWriter:new("network_nodes")
+    local node_and_links_db = shared_state.SharedState:new("nodes_and_links")
+
+    local nodes = {}
+    -- augment the node information from the network_nodes and the 'nodes_and_links' dbs
+    for hostname, node in pairs(network_nodes._nodes_from_db(network_nodes_db)) do
+        if node_and_links_db:get()[hostname] then
+            node.status = "recently_connected"
+        elseif node.member then
+            node.status = "disconnected"
+        else
+            node.status = "gone"
+        end
+        nodes[hostname] = node
+    end
+    return nodes
 end
 
 function network_nodes.mark_nodes_as_gone(hostnames)
-    local sharedState = shared_state.SharedStatePersistent:new("network_nodes")
+    local network_nodes_db = shared_state.SharedStateMultiWriter:new("network_nodes")
+    local nodes = network_nodes._nodes_from_db(network_nodes_db)
     local data = {}
     for _, hostname in pairs(hostnames or {}) do
-        data[hostname] = false
+        local node = nodes[hostname]
+        if node then
+            node.member = false
+            data[hostname] = network_nodes.serialize_for_network_nodes(node)
+        end
     end
-    sharedState:insert(data)
+    network_nodes_db:insert(data)
 end
 
 return network_nodes
--- packages/shared-state-network_nodes/tests/test_network_nodes.lua
+++ packages/shared-state-network_nodes/tests/test_network_nodes.lua
@@ -1,43 +1,89 @@
-local network_nodes = require('network-nodes')
 local utils = require('lime.utils')
 local test_utils = require('tests.utils')
 local shared_state = require('shared-state')
+local network_nodes = require('network-nodes')
 
-describe('Tests network_nodes #network_nodes', function () 
+describe('Tests network_nodes #network_nodes', function ()
     before_each('', function()
         test_dir = test_utils.setup_test_dir()
         shared_state.PERSISTENT_DATA_DIR = test_dir
+        shared_state.DATA_DIR = test_dir
     end)
 
     after_each('', function()
         test_utils.teardown_test_dir()
     end)
 
-    it('test get_nodes return expected format #network_nodes', function()
-        local sharedState = shared_state.SharedStatePersistent:new('network_nodes')
+    it('test node creation and serialization', function()
+        stub(utils, "release_info", function () return {DISTRIB_RELEASE='2021.1'} end)
+        stub(utils, "current_board", function () return 'devboard' end)
+        local node = network_nodes.create_node()
+        assert.are.equal('devboard', node.board)
+        assert.are.equal('2021.1', node.fw_version)
+        assert.are.equal('recently_connected', node.status)
+
+        local node = network_nodes.node("node1", true, "2021.1", "librerouter-v1")
+        assert.are.same(node, network_nodes.deserialize_from_network_nodes(network_nodes.serialize_for_network_nodes(node)))
+
+    end)
+
+    it('test get_nodes return expected format #get_nodes', function()
+        local node1 = network_nodes.node("node1", true, "2021.1", "librerouter-v1")
+        local node2 = network_nodes.node("node2", true, "2020.3", "librerouter-v1")
+        local node3 = network_nodes.node("node3", true, "2020.1", "tplink-wdr3600")
+        local network_nodes_db = shared_state.SharedStateMultiWriter:new('network_nodes')
         local data = {
-            ["node1"] = true,
-            ["node2"] = false,
-            ["node3"] = true,
+            ["node1"] = network_nodes.serialize_for_network_nodes(node1),
+            ["node2"] = network_nodes.serialize_for_network_nodes(node2),
+            ["node3"] = network_nodes.serialize_for_network_nodes(node3),
         }
-        sharedState:insert(data)
-        local expected = {
-            { hostname = "node1", status = "connected"},
-            { hostname = "node2", status = "disconnected"},
-            { hostname = "node3", status = "connected"},
-        }
-        assert.are.same(expected, network_nodes.get_nodes())
+        network_nodes_db:insert(data)
+
+        local nodes_and_links_db = shared_state.SharedState:new('nodes_and_links')
+        nodes_and_links_db:get()
+        nodes_and_links_db:insert({node1={foo='bar'}})
+
+        assert.are.same({node1=node1, node2=node2, node3=node3}, network_nodes._nodes_from_db(network_nodes_db))
+
+        local nodes = network_nodes.get_nodes()
+        assert.is.equal("recently_connected", nodes["node1"].status)
+        assert.is.equal("disconnected", nodes["node2"].status)
+        assert.is.equal("librerouter-v1", nodes["node2"].board)
+
+        local csv = network_nodes.as_csv()
     end)
-    
+
     it('test mark_nodes_as_gone marks nodes as gone', function ()
-        network_nodes.mark_nodes_as_gone({
-            'node1', 'node2', 'node3'
-        })
-        local expected = {
-            { hostname = "node1", status = "disconnected"},
-            { hostname = "node2", status = "disconnected"},
-            { hostname = "node3", status = "disconnected"},
+        local node1 = network_nodes.node("node1", true, "2021.1", "librerouter-v1")
+        local node2 = network_nodes.node("node2", true, "2020.3", "librerouter-v1")
+        local node3 = network_nodes.node("node3", true, "2020.1", "tplink-wdr3600")
+        local network_nodes_db = shared_state.SharedStateMultiWriter:new('network_nodes')
+        local data = {
+            ["node1"] = network_nodes.serialize_for_network_nodes(node1),
+            ["node2"] = network_nodes.serialize_for_network_nodes(node2),
+            ["node3"] = network_nodes.serialize_for_network_nodes(node3),
         }
-        assert.are.same(expected, network_nodes.get_nodes())
+        network_nodes_db:insert(data)
+
+        network_nodes.mark_nodes_as_gone({
+            'node1', 'node2'
+        })
+
+        local nodes = network_nodes.get_nodes()
+        assert.is.equal("gone", nodes["node1"].status)
+        assert.is.equal("gone", nodes["node2"].status)
+        assert.is.equal("disconnected", nodes["node3"].status)
+        assert.is.equal("2020.1", nodes["node3"].fw_version)
     end)
+
+    it('test publish', function ()
+        local hostname = 'mydevpc'
+        stub(utils, "hostname", function () return hostname end)
+        stub(utils, "release_info", function () return {RELEASE='2021.1'} end)
+        stub(utils, "current_board", function () return 'devboard' end)
+        network_nodes.publish()
+        local nodes = network_nodes.get_nodes()
+        assert.is.equal("devboard", nodes[hostname].board)
+    end)
+
 end)
