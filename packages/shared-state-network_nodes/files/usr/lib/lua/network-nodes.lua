c6c7e08cf8ee27713418be220cb6ac32b970cb18
Santiago Piccinini <spiccinini@altermundi.net>
2021-04-05 19:00:02 -0300

    shared-state-network_nodes: add ipv4 and ipv6 data

--- packages/shared-state-network_nodes/files/usr/lib/lua/network-nodes.lua
+++ packages/shared-state-network_nodes/files/usr/lib/lua/network-nodes.lua
@@ -1,18 +1,20 @@
 local shared_state = require("shared-state")
 local utils = require("lime.utils")
+local config = require("lime.config")
 
 local network_nodes = {}
 
-function network_nodes.node(hostname, member, fw_version, board)
-    return {hostname=hostname, member=member, fw_version=fw_version, board=board}
+function network_nodes.node(hostname, member, fw_version, board, ipv4, ipv6)
+    return {hostname=hostname, member=member, fw_version=fw_version, board=board, ipv4=ipv4, ipv6=ipv6}
 end
 
 function network_nodes.serialize_for_network_nodes(node)
-    return {hostname=node.hostname, member=node.member, fw_version=node.fw_version, board=node.board}
+    return {hostname=node.hostname, member=node.member, fw_version=node.fw_version, board=node.board,
+            ipv4=node.ipv4, ipv6=node.ipv6}
 end
 
 function network_nodes.deserialize_from_network_nodes(data)
-    return network_nodes.node(data.hostname, data.member, data.fw_version, data.board)
+    return network_nodes.node(data.hostname, data.member, data.fw_version, data.board, data.ipv4, data.ipv6)
 end
 
 function network_nodes._nodes_from_db(db)
@@ -24,12 +26,18 @@ function network_nodes._nodes_from_db(db)
 end
 
 function network_nodes.create_node()
+    local uci = config.get_uci_cursor()
+
     local hostname = utils.hostname()
     local fw_version = utils.release_info()['DISTRIB_RELEASE']
     local board = utils.current_board()
     local member = true
-    local node = network_nodes.node(hostname, member, fw_version, board)
+    local ipv4 = uci:get("network", "lan", "ipaddr")
+    local ipv6 = uci:get("network", "lan", "ip6addr")
+    if ipv6 then ipv6 = ipv6:gsub("/.*$", "") end -- remove the netmask info
+    local node = network_nodes.node(hostname, member, fw_version, board, ipv4, ipv6)
     node.status = "recently_connected"
+
     return node
 end
 
@@ -42,13 +50,14 @@ function network_nodes.publish()
     network_nodes_db:insert(data)
 end
 
-function network_nodes.as_csv()
+function network_nodes.as_human_readable_table()
     local nodes = network_nodes.get_nodes()
-    local tmpl = "%-30s %-40s %-30s %-20s\n"
-    local out = string.format(tmpl, "hostname", "board", "fw_version", "status")
+    local tmpl = "%-26s %-16s %-30s %-20s %-30s %-40s\n"
+    local out = string.format(tmpl, "hostname", "ipv4", "ipv6", "status", "board", "fw_version")
     for _, node in pairs(nodes) do
         if node.member then
-            out = out .. string.format(tmpl, node.hostname, node.board, node.fw_version, node.status)
+            out = out .. string.format(tmpl, node.hostname, node.ipv4, node.ipv6, node.status,
+                                       node.board, node.fw_version)
         end
     end
     return out
