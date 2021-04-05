06399e5de7fb7b9d3cf13c19f7bf70accb11299a
Daniel Golle <daniel@makrotopia.org>
2021-04-05 14:56:44 +0100

    ping6retrocompat.mk: use wildcard instead of git history
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>

--- ping6retrocompat.mk
+++ ping6retrocompat.mk
@@ -6,15 +6,9 @@
 # and PING6_SYMLINK as you need.
 # See shared-state and lime-debug for usage examples.
 
-# Just in case the the OpenWrt source code is a shallow clone
-$(shell git -C $(TOPDIR) fetch --unshallow || true)
-
-# In case the iputils-ping6 removal commit
-# 98b3526bf23e8d1b48939c937c9b12e4f2160415 is ancestor of current commit
-# ping6 is provided by iputils-ping + PING_LEGACY_SYMLINKS.
-PING6_PACKAGE=iputils-ping6
-PING6_SYMLINK=PACKAGE_iputils-ping6
-ifeq ($(shell git -C $(TOPDIR) merge-base --is-ancestor 98b3526bf23e8d1b48939c937c9b12e4f2160415 HEAD ; echo $$?),0)
 PING6_PACKAGE=iputils-ping
 PING6_SYMLINK=PING_LEGACY_SYMLINKS
+ifeq ($(wildcard $(TOPDIR)/feeds/packages/net/iputils/.),)
+PING6_PACKAGE=iputils-ping6
+PING6_SYMLINK=PACKAGE_iputils-ping6
 endif
