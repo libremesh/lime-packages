dfacfefbca3a23181bf6c79368afdcf2a05e49eb
Santiago Piccinini <spiccinini@altermundi.net>
2021-12-23 13:49:27 -0300

    pirania: distributed portal content

--- packages/pirania/files/usr/lib/lua/portal/portal.lua
+++ packages/pirania/files/usr/lib/lua/portal/portal.lua
@@ -1,7 +1,10 @@
 local utils = require('lime.utils')
 local config = require('lime.config')
+local shared_state = require("shared-state")
 
 local portal = {}
+
+portal.PAGE_CONTENT_OBJ_PATH = '/etc/pirania/portal.json'
 
 function portal.get_config()
     local uci = config.get_uci_cursor()
@@ -29,6 +32,29 @@ function portal.set_config(activated, with_vouchers)
     return true
 end
 
+function portal.get_config()
+    local uci = config.get_uci_cursor()
+    local activated = false
+    if uci:get("pirania", "base_config", "enabled") == '1' then
+       activated = true
+    end
+    return {activated = activated, with_vouchers = true}
+end
 
+function portal.get_page_content()
+    local db = shared_state.SharedStateMultiWriter:new('pirania_persistent'):get()
+    if db.portal then
+        return db.portal.data
+    else
+        return utils.read_obj_store(portal.PAGE_CONTENT_OBJ_PATH)
+    end
+end
+
+
+function portal.set_page_content(title, main_text, logo, link_title, link_url, background_color)
+    local data = {title=title, main_text=main_texst, logo=logo, link_title=link_title, link_url=link_url, background_color=background_color}
+    local db = shared_state.SharedStateMultiWriter:new('pirania_persistent')
+    return db:insert({portal=data})
+end
 
 return portal
--- packages/pirania/tests/test_portal.lua
+++ packages/pirania/tests/test_portal.lua
@@ -19,30 +19,47 @@ describe('Pirania portal tests #portal', function()
         local status = portal.set_config(activated, with_vouchers)
         assert.stub.spy(utils.unsafe_shell).was.called_with('captive-portal start')
         assert.is_true(status)
-        
+
         portal_cfg = portal.get_config()
         assert.is_true(portal_cfg.activated)
-        
+
         local activated = false
         status = portal.set_config(activated, with_vouchers)
         assert.is_true(status)
         assert.stub.spy(utils.unsafe_shell).was.called_with('captive-portal stop')
-        
+
         portal_cfg = portal.get_config()
         assert.is_false(portal_cfg.activated)
-        
+
         with_vouchers = false
         status, message = portal.set_config(activated, with_vouchers)
         assert.is_nil(status)
     end)
 
+    it('get and set portal page', function()
+        stub(utils, "read_obj_store", function() return {title = "Pirania"} end)
+
+        local content = portal.get_page_content()
+        assert.is.equal('Pirania', content.title)
+        local title, main_text, logo, link_title, link_url, bgcolor = 'My Portal', 'my text', 'mylogo', 'linktitle', 'http://foo', '#aabbcc'
+        portal.set_page_content(title, main_text, logo, link_title, link_url, bgcolor)
+
+        local content = portal.get_page_content()
+        assert.are.same({title=title, background_color=bgcolor, link_title=link_title, link_url=link_url, logo=logo}, content)
+
+    end)
+
     before_each('', function()
         snapshot = assert:snapshot()
+        test_dir = test_utils.setup_test_dir()
+        shared_state.PERSISTENT_DATA_DIR = test_dir
+        shared_state.DATA_DIR = test_dir
         uci = test_utils.setup_test_uci()
     end)
 
     after_each('', function()
         snapshot:revert()
+        test_utils.teardown_test_dir()
         test_utils.teardown_test_uci(uci)
     end)
 
