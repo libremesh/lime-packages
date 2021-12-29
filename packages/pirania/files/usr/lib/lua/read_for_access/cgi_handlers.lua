42e97dc0449b69172ff781f945f6c668b351e2eb
German Ferrero <ferrero.gf@gmail.com>
2021-12-29 12:24:58 -0300

    pirania: add test for authorize_mac handler

--- packages/pirania/files/usr/lib/lua/read_for_access/cgi_handlers.lua
+++ packages/pirania/files/usr/lib/lua/read_for_access/cgi_handlers.lua
@@ -1,21 +1,21 @@
 local utils = require('voucher.utils')
 local read_for_access = require('read_for_access.read_for_access')
 local portal = require('portal.portal')
+local config = require('lime.config')
 
 handlers = {}
-local TESTING_URL_AUTHENTICATED = '/authenticated'
 
 function handlers.authorize_mac()
-    local uci_cursor = require('uci').cursor()
+    local uci = config.get_uci_cursor()
     local with_vouchers = portal.get_config().with_vouchers
     if with_vouchers then
-        return uci_cursor:get("pirania", "base_config", "url_auth")
+        return uci:get("pirania", "base_config", "url_auth")
     end
     local client_data = utils.getIpv4AndMac(os.getenv('REMOTE_ADDR'))
     read_for_access.authorize_mac(client_data.mac)
     local params = utils.urldecode_params(os.getenv("QUERY_STRING"))
     local url_prev = utils.urldecode(params['prev'])
-    local url_authenticated = uci_cursor:get("pirania", "base_config", "url_authenticated") or TESTING_URL_AUTHENTICATED
+    local url_authenticated = uci:get("pirania", "base_config", "url_authenticated")
     return url_prev or url_authenticated
 end
 
--- packages/pirania/tests/test_cgi_handlers.lua
+++ packages/pirania/tests/test_cgi_handlers.lua
@@ -3,6 +3,8 @@ local config = require('voucher.config')
 local vouchera = require('voucher.vouchera')
 local handlers = require('voucher.cgi_handlers')
 local utils = require('voucher.utils')
+local rfa_handlers = require('read_for_access.cgi_handlers')
+local read_for_access = require('read_for_access.read_for_access')
 require('packages/pirania/tests/pirania_test_utils').fake_for_tests()
 
 function utils.log(...)
@@ -130,4 +132,55 @@ describe('Vouchera tests #piraniahandlers', function()
         p:close()
     end)
 
+end)
+
+
+describe('read_for_access cgi_handler authorize_mac #readforaccess', function()
+    local snapshot
+    local uci
+    local CONFIG_PATH = "./packages/pirania/files/etc/config/pirania"
+
+    it('calls authorize_mac with the mac from the arp table for the client IP', function()
+        rfa_handlers.authorize_mac()
+        assert.stub(read_for_access.authorize_mac).was_called_with(
+            'AA:BB:CC:DD:EE:FF'
+        )
+    end)
+    
+    it("doesn't call authorize_mac if vouchers are being used", function()
+        uci:set('pirania', 'base_config', 'with_vouchers', "1")
+        rfa_handlers.authorize_mac()
+        assert.stub(read_for_access.authorize_mac).was_not.called()
+    end)
+    
+    it("return prev url if there is one", function()
+        local original_url = 'http://original.url/baz?a=1&b=2'
+        local querystring = "?prev="..utils.urlencode(original_url)
+        stub(os, "getenv", build_env('10.1.1.1', querystring))
+        local url = rfa_handlers.authorize_mac()
+        assert.is_equal(original_url, url)
+    end)
+
+    it("return authenticated url if there is no prev url", function()
+        stub(os, "getenv", build_env('10.1.1.1'))
+        local url = rfa_handlers.authorize_mac()
+        local url_authenticated = uci:get('pirania', 'base_config', 'url_authenticated')
+        assert.is_equal(url_authenticated, url)
+    end)
+    
+    before_each('', function()
+        snapshot = assert:snapshot()
+        uci = test_utils.setup_test_uci()
+        local default_cfg = io.open(CONFIG_PATH):read("*all")
+        test_utils.write_uci_file(uci, 'pirania', default_cfg)
+        stub(os, "getenv",  build_env('10.1.1.1'))
+        stub(utils, "getIpv4AndMac", function () return {mac='AA:BB:CC:DD:EE:FF', ip='10.1.1.1'} end)
+        stub(read_for_access, "authorize_mac", function() return end)
+        uci:set('pirania', 'base_config', 'with_vouchers', "0")
+    end)
+    
+    after_each('', function()
+        snapshot:revert()
+        test_utils.teardown_test_uci(uci)
+    end)
 end)
