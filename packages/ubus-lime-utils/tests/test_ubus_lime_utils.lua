3c1fe0c30bc8fc5546a190e39da5152b433ca6fd
selankon <selankon@selankon.xyz>
2023-01-27 09:54:19 +0100

    Write tests for get_most_active

--- packages/ubus-lime-utils/tests/test_ubus_lime_utils.lua
+++ packages/ubus-lime-utils/tests/test_ubus_lime_utils.lua
@@ -3,9 +3,12 @@ local test_utils = require "tests.utils"
 local hotspot_wwan = require 'lime.hotspot_wwan'
 local limeutils = require "lime-utils"
 local json = require 'luci.jsonc'
+local node_status = require 'lime.node_status'
 
 local uci
 local snapshot -- to revert luassert stubs and spies
+
+local mocks = {}
 
 describe('ubus-lime-utils tests #ubuslimeutils', function()
 
@@ -51,6 +54,23 @@ describe('ubus-lime-utils tests #ubuslimeutils', function()
         assert.is.equal("123", response.uptime)
     end)
 
+
+    it('test get_most_active return most active iface with stats from iw', function()
+        stub(utils, "unsafe_shell", function (cmd) 
+            if string.match(cmd, "wlan0") then 
+                return mocks.iw_station_get_result_wlan0
+            end
+            return mocks.iw_station_get_result_wlan1
+        end
+        )
+        stub(node_status, "get_stations", function () return mocks.get_stations end)
+        local most_active = node_status.get_most_active()
+        assert.is.equal("wlan0-mesh", most_active["iface"])
+        assert.is.equal("13 [10, 11] dBm", most_active["signal"])
+        assert.is.equal(3116498, most_active["rx_bytes"])
+        assert.is.equal(1166333, most_active["tx_bytes"])
+    end)
+
     it('test get_upgrade_info', function()
         stub(utils, "unsafe_shell", function () return '-1' end)
         stub(os, "execute", function () return '0' end)
@@ -94,3 +114,130 @@ describe('ubus-lime-utils tests #ubuslimeutils', function()
         test_utils.teardown_test_uci(uci)
     end)
 end)
+
+
+mocks.iw_station_get_result_wlan1 = [[
+Station c0:4a:00:be:7b:0a (on wlan1-mesh)
+    inactive time:  50 ms
+    rx bytes:  503044
+    rx packets:  3976
+    tx bytes:  545116
+    tx packets:  1237
+    tx retries:  9
+    tx failed:  0
+    rx drop misc:  3
+    signal:    -14 [-17, -16] dBm
+    signal avg:  -12 [-14, -15] dBm
+    Toffset:  46408315 us
+    tx bitrate:  300.0 MBit/s MCS 15 40MHz short GI
+    rx bitrate:  300.0 MBit/s MCS 15 40MHz short GI
+    rx duration:  0 us
+    expected throughput:  58.43Mbps
+    mesh llid:  5944
+    mesh plid:  1241
+    mesh plink:  ESTAB
+    mesh local PS mode:  ACTIVE
+    mesh peer PS mode:  ACTIVE
+    mesh non-peer PS mode:  ACTIVE
+    authorized:  yes
+    authenticated:  yes
+    associated:  yes
+    preamble:  long
+    WMM/WME:  yes
+    MFP:    no
+    TDLS peer:  no
+    DTIM period:  2
+    beacon interval:100
+    short slot time:yes
+    connected time:  139 seconds
+]]
+
+
+mocks.iw_station_get_result_wlan0 = [[
+    Station c0:4a:00:be:7b:09 (on wlan0-mesh)
+	inactive time:	140 ms
+	rx bytes:	3116498
+	rx packets:	31613
+	tx bytes:	1166333
+	tx packets:	4462
+	tx retries:	2448
+	tx failed:	15
+	rx drop misc:	938
+	signal:  	13 [10, 11] dBm
+	signal avg:	25 [-28, -15] dBm
+	Toffset:	18446744073577465064 us
+	tx bitrate:	6.5 MBit/s MCS 0
+	rx bitrate:	39.0 MBit/s MCS 10
+	rx duration:	0 us
+	expected throughput:	2.197Mbps
+	mesh llid:	63041
+	mesh plid:	61249
+	mesh plink:	ESTAB
+	mesh local PS mode:	ACTIVE
+	mesh peer PS mode:	ACTIVE
+	mesh non-peer PS mode:	ACTIVE
+	authorized:	yes
+	authenticated:	yes
+	associated:	yes
+	preamble:	long
+	WMM/WME:	yes
+	MFP:		no
+	TDLS peer:	no
+	DTIM period:	2
+	beacon interval:100
+	short slot time:yes
+	connected time:	5070 seconds
+]]
+
+
+
+mocks.get_stations = {
+    [1] = {
+        ["rx_short_gi"] = false,
+        ["station_mac"] = "C0:4A:00:BE:7B:09",
+        ["rx_vht"] = false,
+        ["rx_mhz"] = 20,
+        ["rx_40mhz"] = false,
+        ["tx_packets"] = 1574,
+        ["tx_mhz"] = 20,
+        ["rx_packets"] = 16879,
+        ["rx_ht"] = true,
+        ["tx_mcs"] = 9,
+        ["noise"] = -95,
+        ["rx_mcs"] = 1,
+        ["tx_ht"] = true,
+        ["iface"] = "wlan0-mesh",
+        ["tx_rate"] = 26000,
+        ["inactive"] = 1390,
+        ["tx_short_gi"] = false,
+        ["tx_40mhz"] = false,
+        ["expected_throughput"] = 11437,
+        ["tx_vht"] = false,
+        ["rx_rate"] = 13000,
+        ["signal"] = 13
+      },
+    [2] = {
+        ["rx_short_gi"] = true,
+        ["station_mac"] = "C0:4A:00:BE:7B:0A",
+        ["rx_vht"] = false,
+        ["rx_mhz"] = 40,
+        ["rx_40mhz"] = true,
+        ["tx_packets"] = 7078,
+        ["tx_mhz"] = 40,
+        ["rx_packets"] = 54294,
+        ["rx_ht"] = true,
+        ["tx_mcs"] = 15,
+        ["noise"] = -91,
+        ["rx_mcs"] = 15,
+        ["tx_ht"] = true,
+        ["iface"] = "wlan1-mesh",
+        ["tx_rate"] = 300000,
+        ["inactive"] = 70,
+        ["tx_short_gi"] = true,
+        ["tx_40mhz"] = true,
+        ["expected_throughput"] = 59437,
+        ["tx_vht"] = false,
+        ["rx_rate"] = 300000,
+        ["signal"] = -13
+      }
+}
