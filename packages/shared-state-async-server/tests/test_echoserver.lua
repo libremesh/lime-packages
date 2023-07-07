f6128c32fd260a5ebfa65dc7e8312f8f0ab3b69c
javier.jorge <javier.jorge@unc.edu.ar>
2023-07-07 10:45:22 -0300

    addedtest

--- packages/shared-state-async-server/tests/test_echoserver.lua
+++ packages/shared-state-async-server/tests/test_echoserver.lua
@@ -6,7 +6,46 @@ local interval = 10
 local function debug(...)
 	print(socket.gettime(), unpack(arg))
 end
-function push_loop()
+
+local function get(collectors_output)
+	local tcp, status, err
+	if tcp then
+		tcp:close()
+	end
+	tcp = assert(socket.tcp6())
+	tcp:settimeout(10) -- seconds
+	debug("### try connecting to " .. server_address .. " over IPv6")
+	status, err = tcp:connect(server_address, server_port)
+	if not status then -- retry falling back to ipv4
+		debug("tcp status:", status, "err:", err)
+		debug("# retry connecting to " .. server_address .. " over IPv4")
+		tcp = assert(socket.tcp())
+		tcp:settimeout(10) -- seconds
+		status, err = tcp:connect(server_address, server_port)
+	end
+	debug("tcp status:", status, "err:", err)
+	returnstring = ""
+	if status then
+		debug("sending ... : " .. string.len(collectors_output) )
+		status, err, last_byte = tcp:send(collectors_output)
+		debug('### sent: ', status, err, last_byte)
+		debug('### receive: ')
+		while true do
+			s, status, partial = tcp:receive('*a')
+			if status == "closed" then
+				break
+			end
+			returnstring=s
+			print("s " ..returnstring)
+		end
+		print("s " ..returnstring)
+		debug('### end receive')
+	end
+	tcp:close()
+	return returnstring
+end
+
+local function gethttp()
 	local tcp, status, err
 	local start_time = socket.gettime()
 
@@ -28,6 +67,7 @@ function push_loop()
 		tcp:settimeout(10) -- seconds
 		status, err = tcp:connect(server_address, server_port)
 	end
+	
 	debug("tcp status:", status, "err:", err)
 
 	if status then
@@ -60,9 +100,10 @@ function push_loop()
 	return 4
 end
 
-describe('foo library tests', function()
-    it('very simple test of f_sum(a, b)', function()
-        assert.is.equal(4, push_loop())
-		assert.is.equal(5, push_loop())
+describe('test async echo server', function()
+    it('sends a piece of texh and expects to get it back', function()
+        assert.is.equal("hola", get("hola"))
+		assert.is.equal("holaholaholaholaholaholaholaholaholaholaholaholaholaholaholahola", get("holaholaholaholaholaholaholaholaholaholaholaholaholaholaholahola"))
     end)
 end)
+
