e9aadbdf2fe203715f494a268ba9f8f25999f924
javierbrk <javierbrk@gmail.com>
2023-10-20 16:33:15 -0300

    wip ...

--- packages/lime-mesh-upgrade/files/usr/lib/lua/lime-mesh-upgrade.lua
+++ packages/lime-mesh-upgrade/files/usr/lib/lua/lime-mesh-upgrade.lua
@@ -6,4 +6,12 @@ function mesh_upgrade.became_master_node(urls)
     -- todo
 end
 
+function mesh_upgrade.upgrade_in_progress()
+    return uci:get('mesh_upgrade', 'main', 'transaction_state') == 'started'
+end
+
+function mendmesh_upgrade.start(upgrade_data)
+end
+
+
 return mesh_upgrade
