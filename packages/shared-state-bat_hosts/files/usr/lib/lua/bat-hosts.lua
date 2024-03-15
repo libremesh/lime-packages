d7a3873fd81fb64093f6d586e0de6901a58193e1
javierbrk <javierbrk@gmail.com>
2024-03-15 15:18:05 -0300

    bat_hosts: fixes rpcd error

--- packages/shared-state-bat_hosts/files/usr/lib/lua/bat-hosts.lua
+++ packages/shared-state-bat_hosts/files/usr/lib/lua/bat-hosts.lua
@@ -1,4 +1,3 @@
-local shared_state = require("shared-state")
 local JSON = require("luci.jsonc")
 local utils = require("lime.utils")
 
@@ -10,7 +9,7 @@ function bat_hosts.bathost_deserialize(hostname_plus_iface)
     for _, ifname in ipairs(utils.get_ifnames()) do
         local serialized_ifname = string.gsub(ifname, "%W", "_")
         serialized_ifname = utils.literalize(serialized_ifname)
-        local replaced_hostname = hostname_plus_iface:gsub("_"..serialized_ifname, "")
+        local replaced_hostname = hostname_plus_iface:gsub("_" .. serialized_ifname, "")
         -- hostname don't have underscores see utils.is_valid_hostname
         replaced_hostname = replaced_hostname:gsub("_", "-")
         if #replaced_hostname < #partial_hostname then
@@ -21,13 +20,17 @@ function bat_hosts.bathost_deserialize(hostname_plus_iface)
     return partial_hostname, iface
 end
 
+function bat_hosts.get_bat_hosts_from_shared_state()
+    return JSON.parse(
+        io.popen("shared-state-async get bat-hosts 2> /dev/null", "r"):read("*all"))
+end
+
 function bat_hosts.get_bathost(mac, outgoing_iface)
-	local bathosts = JSON.stringify(
-		io.popen("shared-state-async get bat-hosts", "r"):read("*all") )
-	local bathost = bathosts[mac:lower()]
-	if bathost == nil then return end
-	local hostname, iface = bat_hosts.bathost_deserialize(bathost.data)
-	return { hostname = hostname, iface = iface }
+    local bathosts = bat_hosts.get_bat_hosts_from_shared_state()
+    local bathost = bathosts[mac:lower()]
+    if bathost == nil then return end
+    local hostname, iface = bat_hosts.bathost_deserialize(bathost)
+    return { hostname = hostname, iface = iface }
 end
 
 return bat_hosts
