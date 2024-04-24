4b16bf8c2a0d58be8ed5affaf934a3d15ad7a005
javierbrk <javierbrk@gmail.com>
2024-04-24 09:45:01 -0300

    shared-state: enables publish after reboot

--- packages/shared-state-bat_hosts/files/etc/hotplug.d/iface/shared-state-bat_hosts.sh
+++ packages/shared-state-bat_hosts/files/etc/hotplug.d/iface/shared-state-bat_hosts.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-[ "x$ACTION" == "xifup" ] && ((sleep 30; shared-state-publish_bat_hosts; shared-state-async sync bat-hosts)&)
+[ "x$ACTION" == "xifup" ] && ((sleep 30; /usr/share/shared-state/publishers/shared-state-publish_bat_hosts; shared-state-async sync bat-hosts)&)
