libremesh/lime-packages

commit 606ae4c6b02118571a3d312b6426d1c57e4e796c

ubus-admin: set_password sets the shared password

packages/lime-system/files/usr/lib/lua/lime/utils.lua
@@ -326,6 +326,13 @@ function utils.set_root_secret(secret)
 	end
 end
 
+function utils.set_shared_root_password(password)
+    local uci = config.get_uci_cursor()
+    utils.set_password('root', password) -- this takes 1 second, it may be replaced with nixio.crypt(password, '$1$vv44cu1H')
+    uci:set("lime-community", 'system', 'root_password_policy', 'SET_SECRET')
+    uci:set("lime-community", 'system', 'root_password_secret', utils.get_root_secret())
+end
+
 --! returns a random string. filter is an optional function to reduce the possible characters.
 --! by default the filter allows all the alphanumeric characters
 function utils.random_string(length, filter)

packages/first-boot-wizard/files/usr/lib/lua/firstbootwizard.lua
@@ -261,9 +261,7 @@ function fbw.create_network(ssid, hostname, password)
 
     -- Save changes in lime-community
     if password ~= nil and password ~= '' then
-        lutils.set_password('root', password) -- this takes 1 second, it may be replaced with nixio.crypt(password, '$1$vv44cu1H')
-        uci_cursor:set("lime-community", 'system', 'root_password_policy', 'SET_SECRET')
-        uci_cursor:set("lime-community", 'system', 'root_password_secret', lutils.get_root_secret())
+        lutils.set_shared_root_password(password)
     end
     uci_cursor:set("lime-community", 'wifi', 'ap_ssid', ssid)
     uci_cursor:set("lime-community", 'wifi', 'apname_ssid', ssid..'/%H')

packages/first-boot-wizard/tests/test_firstbootwizard.lua
@@ -82,6 +82,7 @@ describe('FirstBootWizard tests #fbw', function()
 
         assert.is.equal('SET_SECRET', uci:get("lime-community", 'system', 'root_password_policy'))
         assert.is.equal('mysecret', uci:get("lime-community", 'system', 'root_password_secret'))
+        assert.stub(utils.set_password).was.called_with('root', "mypassword")
     end)
 
     before_each('', function()

packages/lime-system/tests/test_lime_utils.lua
@@ -1,6 +1,8 @@
 local utils = require 'lime.utils'
 local test_utils = require 'tests.utils'
 
+local uci = nil
+
 describe('LiMe Utils tests #limeutils', function()
     it('test literalize(str) with a string that has all the reserved chars', function()
         local str = 'f+o[o]?.*(,)_-%a$l^'
@@ -165,10 +167,29 @@ dnsmasq:*:18362:0:99999:7:::
 		assert.is.equal(shadow_content_with_old_password, utils.read_file(TEST_SHADOW_FILENAME .. "-"))
 	end)
 
+    it('test set_shared_root_password', function()
+		uci:set('lime-community', 'system', 'lime')
+		stub(utils, "set_password", function (user, pass) return pass end)
+
+		utils.set_shared_root_password('foo')
+
+		assert.stub(utils.set_password).was.called_with('root', 'foo')
+		assert.is.equal("SET_SECRET", uci:get("lime-community", 'system', 'root_password_policy'))
+		assert.is_not("", uci:get("lime-community", 'system', 'root_password_secret'))
+	end)
+
 	it('test random_string', function()
 		assert.is.equal(5, #utils.random_string(5))
 		assert.is.not_equal(utils.random_string(5), utils.random_string(5))
 		assert.is.equal("number", type(tonumber(utils.random_string(5, function (c) return c:match('%d') ~= nil end))))
 	end)
+
+    before_each('', function()
+        uci = test_utils.setup_test_uci()
+    end)
+
+    after_each('', function()
+        test_utils.teardown_test_uci(uci)
+    end)
 
 end)

packages/ubus-lime-utils/files/usr/libexec/rpcd/lime-utils-admin
@@ -20,7 +20,7 @@ local function set_root_password(msg)
     if type(msg.password) ~= "string" then
         result = {status = 'error', msg = 'Password must be a string'}
     else
-        utils.set_password('root', msg.password or '')
+        utils.set_shared_root_password(msg.password or '')
         result = {status = 'ok'}
     end
     return utils.printJson(result)

packages/ubus-lime-utils/tests/test_lime_utils_admin.lua
@@ -15,11 +15,15 @@ describe('ubus-lime-utils-admin tests #ubuslimeutilsadmin', function()
     end)
 
     it('test set_root_password', function()
+        uci:set('lime-community', 'system', 'lime')
         stub(utils, "set_password", function (user, pass) return pass end)
+
         local response  = rpcd_call(ubus_lime_utils, {'call', 'set_root_password'},
                                     '{"password": "foo"}')
+
         assert.is.equal("ok", response.status)
         assert.stub(utils.set_password).was.called_with('root', 'foo')
+        assert.is.equal("SET_SECRET", uci:get("lime-community", 'system', 'root_password_policy'))
     end)
 
     before_each('', function()

commit b0ac255f13c99c4fdfd557a200371e01f01f85c2

fix(utils): add missing UCI commit in set_shared_root_password
Fixes password changes not persisting when using network-admin password
change feature. The function was updating UCI but never committing,
causing changes to be lost on reboot or config reload.

The bug existed since the function was introduced in commit 606ae4c6
(2020). It went unnoticed because First Boot Wizard did commit after
calling this function, but network-admin relies on the function to
commit its own changes.

Impact: Without this fix, mesh-wide password changes don't work - not
even on the local node where the change is initiated.

Tested: All tests pass (306 successes / 0 failures)

AI-assisted development

packages/lime-system/files/usr/lib/lua/lime/utils.lua
@@ -381,6 +381,7 @@ function utils.set_shared_root_password(password)
     utils.set_password('root', password) -- this takes 1 second, it may be replaced with nixio.crypt(password, '$1$vv44cu1H')
     uci:set("lime-community", 'system', 'root_password_policy', 'SET_SECRET')
     uci:set("lime-community", 'system', 'root_password_secret', utils.get_root_secret())
+    uci:commit("lime-community")
 end
 
 --! returns a random string. filter is an optional function to reduce the possible characters.